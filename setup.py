572676fc2cc1a00d378e760f956e32f4eaa19560
mduranfrigola <miquelduranfrigola@gmail.com>
2020-11-30 17:39:30 +0100

    Use python 3.7

--- setup.py
+++ setup.py
@@ -52,7 +52,7 @@ setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     license='MIT',
-    python_requires=">=3.8",
+    python_requires=">=3.7",
     install_requires=install_requires,
     extras_require=extras_require,
     packages=find_packages(exclude=("utilities")),
@@ -62,7 +62,7 @@ setup(
         ]
     },
     classifiers=(
-        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.7",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
         "Topic :: Scientific/Engineering :: Artificial Intelligence"
