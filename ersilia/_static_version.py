93be13f14722817e2b89255c5b9ee0109a2258a5
mduranfrigola <miquelduranfrigola@gmail.com>
2020-12-01 16:00:38 +0100

    moved to miniver instead of versioneer

--- ersilia/_static_version.py
+++ ersilia/_static_version.py
@@ -3,7 +3,7 @@
 #
 # This file will be overwritten by setup.py when a source or binary
 # distribution is made.  The magic value "__use_git__" is interpreted by
-# _version.py.
+# version.py.
 
 version = "__use_git__"
 
--- setup.py
+++ setup.py
@@ -1,16 +1,19 @@
 from setuptools import setup, find_packages
 
-# Loads _version.py module without importing the whole package.
-def get_version_and_cmdclass(package_name):
+def get_version_and_cmdclass(package_path):
+    """Load version.py module without importing the whole package.
+
+    Template code from miniver
+    """
     import os
     from importlib.util import module_from_spec, spec_from_file_location
-    spec = spec_from_file_location('version',
-                                   os.path.join(package_name, '_version.py'))
+
+    spec = spec_from_file_location("version", os.path.join(package_path, "_version.py"))
     module = module_from_spec(spec)
     spec.loader.exec_module(module)
     return module.__version__, module.cmdclass
 
-version, cmdclass = get_version_and_cmdclass('ersilia')
+version, cmdclass = get_version_and_cmdclass("ersilia")
 
 with open("README.md", "r", encoding="utf8") as fh:
     long_description = fh.read()
