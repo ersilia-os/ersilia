8de14161bd3032004a08a237b107e6481d72f96f
miquelduranfrigola <miquelduranfrigola@gmail.com>
2021-01-11 08:03:42 +0100

    removed githubpages extension

--- docs/conf.py
+++ docs/conf.py
@@ -39,7 +39,6 @@ release = ''
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-'githubpages'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
