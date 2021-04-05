f8fa5eda63b8c2dc802f58aa1b6b062c246fd790
miquelduranfrigola <miquelduranfrigola@gmail.com>
2021-04-05 18:05:22 +0100

    timestamp identifier

---
+++ ersilia/utils/identifiers/timestamp.py
@@ -0,0 +1,10 @@
+from datetime import datetime
+
+
+class TimeStampIdentifier(object):
+
+    def __init__(self):
+        self.stamp = datetime.now()
+
+    def encode(self):
+        return self.stamp.strftime("%Y%m%d%H%M%S")
