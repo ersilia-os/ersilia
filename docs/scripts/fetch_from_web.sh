b0a06de607e1b3da56c48a9e858f8ca2f6b8d89d
miquelduranfrigola <miquelduranfrigola@gmail.com>
2021-10-02 21:37:39 +0200

    remove fetched from web

--- docs/scripts/fetch_from_web.sh
+++
@@ -1,6 +0,0 @@
-wget https://raw.githubusercontent.com/ersilia-os/ersilia.io/main/docs/README.md -O ../source/fetched/README.md
-wget https://raw.githubusercontent.com/ersilia-os/ersilia.io/main/docs/installation.md -O ../source/fetched/installation.md
-wget https://raw.githubusercontent.com/ersilia-os/ersilia.io/main/docs/quickstart.md -O ../source/fetched/quickstart.md
-
-wget https://raw.githubusercontent.com/ersilia-os/ersilia.io/main/docs/codeofconduct.md -O ../source/fetched/codeofconduct.md
-wget https://raw.githubusercontent.com/ersilia-os/ersilia.io/main/docs/support-our-project.md -O ../source/fetched/support-our-project.md
