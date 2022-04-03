177e9a736ed67809df55ac0aa44ebf55a7f192a3
Amna Ali <98670206+Amna-28@users.noreply.github.com>
2022-04-04 03:42:19 +0500

    Update __init__.py
    Maintain last_cleaned.json
    check if current_time - last_cleaned_time > 1 week , checks unused models and cleans them up

--- ersilia/cli/commands/__init__.py
+++ ersilia/cli/commands/__init__.py
@@ -4,6 +4,9 @@ from ... import __version__
 from ... import logger
 from ..echo import Silencer
 
+import json
+import csv
+import time
 
 @click.group(cls=BentoMLCommandGroup)
 @click.version_option(version=__version__)
@@ -33,3 +36,21 @@ def ersilia_cli(verbose, silent):
     silencer.speak()  # To reset default
     if silent:
         silencer.silence()
+    
+    ts_dict = {}
+    if not exists('last_cleaned.json'):
+        ts_dict['timestamp'] = str(time.time())
+        with open('last_cleaned.json', 'w') as outfile:
+            json.dump(ts_dict, outfile)
+    else:
+        current_ts = time.time()
+        with open('last_cleaned.json') as json_file:
+            ts_dict = json.load(json_file)
+            ts = float(ts_dict['timestamp'])
+            if(current_ts - ts) > 604800:
+                with open("fetched_models.txt") as infile:
+                    fetched_models = dict(csv.reader(infile))
+                for m_name in fetched_models: 
+                    if( current_ts - float(fetched_models[m_name])) > 2592000:
+                        del_cmd = 'ersilia delete ' + m_name
+                        test_op = os.system(del_cmd)
