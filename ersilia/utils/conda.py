2a2e56c7755f3c29bf2983ac6c68ad5f764e7ff8
Carolina Caballero López <carolina.cl19@gmail.com>
2023-01-03 13:13:51 -0500

    Add funciont getPythonPath_env()
    Add a function to get the python path variable of the conda environment.

--- ersilia/utils/conda.py
+++ ersilia/utils/conda.py
@@ -272,6 +272,12 @@ class SimpleConda(CondaUtils):
             if l.startswith(environment):
                 envs_list += [l.split(" ")[0]]
         return envs_list
+    
+    def getPythonPath_env(name_env):
+        python_path= subprocess.check_output("which python", shell=True).strip()
+        index_env=python_path.decode('utf-8').find("envs/") + 5
+        python_path_envs_model= python_path.decode('utf-8')[0:index_env]
+        return python_path_envs_model + name_env_model + "/bin/python"
 
     def delete_one(self, environment):
         if not self.exists(environment):
