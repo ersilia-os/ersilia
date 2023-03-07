2d249d0077fb0f3e4a4992be0101e12441033f95
Miquel Duran-Frigola <miquelduranfrigola@gmail.com>
2023-03-07 07:57:01 +0100

    skip subfiles if not installed

--- ersilia/hub/content/information.py
+++ ersilia/hub/content/information.py
@@ -24,32 +24,60 @@ class Information(ErsiliaBase):
         self.dest_folder = os.path.join(self._model_path(model_id=model_id))
 
     def _get_pack_mode(self):
-        with open(os.path.join(self.dest_folder, PACKMODE_FILE), "r") as f:
-            return f.read().rstrip()
+        pack_mode_file = os.path.join(self.dest_folder, PACKMODE_FILE)
+        if os.path.exists(pack_mode_file):
+            with open(pack_mode_file, "r") as f:
+                return f.read().rstrip()
+        else:
+            return None
 
     def _get_service_class(self):
-        with open(os.path.join(self.repository_folder, SERVICE_CLASS_FILE), "r") as f:
-            return f.read().rstrip()
+        service_class_file = os.path.join(self.repository_folder, SERVICE_CLASS_FILE)
+        if os.path.exists(service_class_file):
+            with open(service_class_file, "r") as f:
+                return f.read().rstrip()
+        else:
+            return None
 
     def _get_api_schema(self):
-        with open(os.path.join(self.dest_folder, API_SCHEMA_FILE), "r") as f:
-            return json.load(f)
+        api_schema_file = os.path.join(self.dest_folder, API_SCHEMA_FILE)
+        if os.path.exists(api_schema_file):
+            with open(api_schema_file, "r") as f:
+                return json.load(f)
+        else:
+            return None
 
     def _get_size(self):
-        with open(os.path.join(self.dest_folder, MODEL_SIZE_FILE), "r") as f:
-            return json.load(f)
+        size_file = os.path.join(self.dest_folder, MODEL_SIZE_FILE)
+        if os.path.exists(size_file):
+            with open(size_file, "r") as f:
+                return json.load(f)
+        else:
+            return None
 
     def _get_metadata(self):
-        with open(os.path.join(self.dest_folder, METADATA_JSON_FILE), "r") as f:
-            return json.load(f)
+        metadata_file = os.path.join(self.dest_folder, METADATA_JSON_FILE)
+        if os.path.exists(metadata_file):
+            with open(metadata_file, "r") as f:
+                return json.load(f)
+        else:
+            return None
 
     def _get_card(self):
-        with open(os.path.join(self.dest_folder, CARD_FILE), "r") as f:
-            return json.load(f)
+        card_file = os.path.join(self.dest_folder, CARD_FILE)
+        if os.path.exists(card_file):
+            with open(card_file, "r") as f:
+                return json.load(f)
+        else:
+            return None
 
     def _get_apis_list(self):
-        with open(os.path.join(self.repository_folder, APIS_LIST_FILE), "r") as f:
-            return [x.rstrip() for x in f.readlines()]
+        apis_list_file = os.path.join(self.dest_folder, CARD_FILE)
+        if os.path.exists(apis_list_file):
+            with open(os.path.join(self.repository_folder, APIS_LIST_FILE), "r") as f:
+                return [x.rstrip() for x in f.readlines()]
+        else:
+            return None
 
     def get(self):
         data = {
