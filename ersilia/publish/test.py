a64ed134f8e145304cf0655c43e3f83190495467
Riley Pittman <rileypittman@DNa80955a.SUNet>
2023-07-25 14:36:01 -0700

    Test module updates

--- ersilia/publish/test.py
+++ ersilia/publish/test.py
@@ -3,7 +3,7 @@ import json
 import click
 import tempfile
 import types
-import time
+import subprocess
 from ..cli import echo
 
 from ..io.input import ExampleGenerator
@@ -15,7 +15,16 @@ from ..utils.exceptions_utils import test_exceptions as texc
 from ..core.session import Session
 
 from ..default import INFORMATION_FILE
+
+RUN_FILE = "run.sh"
+DATA_FILE = "data.csv"
+OUTPUT_FILE = "output.csv"
+FRAMEWORK_BASEDIR = "framework"
+DIFFERENCE_THRESHOLD = 5    # outputs should be within this percent threshold to be considered consistent
 NUM_SAMPLES = 5
+BOLD = '\033[1m'
+RESET = '\033[0m'
+RED = '\033[31m'
 
 class ModelTester(ErsiliaBase):
     def __init__(self, model_id, config_json=None):
@@ -25,6 +34,7 @@ class ModelTester(ErsiliaBase):
         self._info = self._read_information()
         self._input = self._info["card"]["Input"]
         self._prepare_input_files()
+        self.RUN_FILE = "run.sh"
 
     def _read_information(self):
         json_file = os.path.join(self._dest_dir, self.model_id, INFORMATION_FILE)
@@ -43,6 +53,7 @@ class ModelTester(ErsiliaBase):
     This helper method was taken from the run.py file, and just prints the output for the user 
     """
     def _print_output(self, result): 
+        echo("Printing output...")
         if isinstance(result, types.GeneratorType):
             for r in result:
                 if r is not None:
@@ -51,20 +62,151 @@ class ModelTester(ErsiliaBase):
                     echo("Something went wrong", fg="red")
         else:
             echo(result)
+    
+
+    """
+    This helper method checks that the model ID is correct.
+    """
+    def _check_model_id(self, data): 
+        print("Checking model ID...")
+        if data["card"]["Identifier"] != self.model_id:
+            raise texc.WrongCardIdentifierError(self.model_id)    
 
 
     """
-    Check the model information to make sure it's correct. Need to add more here.
+    This helper method checks that the slug field is non-empty.
+    """
+    def _check_model_slug(self, data): 
+        print("Checking model slug...")
+        if not data["card"]["Slug"]:
+            raise texc.EmptyField("slug")
+   
+    """
+    This helper method checks that the description field is non-empty.
+    """
+    def _check_model_description(self, data): 
+        print("Checking model description...")
+        if not data["card"]["Description"]:
+            raise texc.EmptyField("Description")
+
+    """
+    This helper method checks that the model task is one of the following valid entries:
+        - Classification
+        - Regression
+        - Generative
+        - Representation
+        - Similarity
+        - Clustering
+        - Dimensionality reduction
+    """
+    def _check_model_task(self, data): 
+        print("Checking model task...")
+        valid_tasks = [[ 'Classification'], [ 'Regression' ], [ 'Generative' ], [ 'Representation' ], 
+                       [ 'Similarity' ], [ 'Clustering' ], [ 'Dimensionality reduction' ]]
+        if data["card"]["Task"] not in valid_tasks:
+            raise texc.InvalidEntry("Task")
+    
+    """
+    This helper method checks that the input field is one of the following valid entries:
+        - Compound
+        - Protein
+        - Text
+    """
+    def _check_model_input(self, data): 
+        print("Checking model input...")
+        valid_inputs = [[ 'Compound' ], [ 'Protein' ], [ 'Text' ]]
+        if data["card"]["Input"] not in valid_inputs:
+            raise texc.InvalidEntry("Input")
+
+    """
+    This helper method checks that the input shape field is one of the following valid entries:
+        - Single
+        - Pair
+        - List
+        - Pair of Lists
+        - List of Lists
+    """
+    def _check_model_input_shape(self, data): 
+        print("Checking model input shape...")
+        valid_input_shapes = ["Single", "Pair", "List", "Pair of Lists", "List of Lists"]
+        if data["card"]["Input Shape"] not in valid_input_shapes:
+            raise texc.InvalidEntry("Input Shape")
+
+    """
+    This helper method checks the the output is one of the following valid entries:
+        - Boolean
+        - Compound
+        - Descriptor
+        - Distance
+        - Experimental value
+        - Image
+        - Other value
+        - Probability
+        - Protein
+        - Score
+        - Text
+    """
+    def _check_model_output(self, data): 
+        print("Checking model output...")
+        valid_outputs = [[ 'Boolean' ], [ 'Compound' ], [ 'Descriptor' ], [ 'Distance' ], [ 'Experimental value' ], 
+                         [ 'Image' ], [ 'Other value' ], [ 'Probability' ], [ 'Protein' ], [ 'Score' ], [ 'Text' ]]
+        if data["card"]["Output"] not in valid_outputs:
+            raise texc.InvalidEntry("Output")
+
+    """
+    This helper method checks that the output type is one of the following valid entries:
+        - String
+        - Float
+        - Integer
+    """
+    def _check_model_output_type(self, data): 
+        print("Checking model output type...")
+        valid_output_types = [[ 'String' ], [ 'Float' ], [ 'Integer' ]]
+        if data["card"]["Output Type"] not in valid_output_types:
+            raise texc.InvalidEntry("Output Type")
+
+    """
+    This helper method checks that the output shape is one of the following valid entries:
+        - Single
+        - List
+        - Flexible List
+        - Matrix
+        - Serializable Object
+    """
+    def _check_model_output_shape(self, data): 
+        print("Checking model output shape...")
+        valid_output_shapes = ["Single", "List", "Flexible List", "Matrix", "Serializable Object"]
+        if data["card"]["Output Shape"] not in valid_output_shapes:
+            raise texc.InvalidEntry("Output Shape")
+
+    """
+    Check the model information to make sure it's correct. Performs the following checks:
+    - Checks that model ID is correct
+    - Checks that model slug is non-empty
+    - Checks that model description is non-empty
+    - Checks that the model task is valid
+    - Checks that the model input, input shape is valid
+    - Checks that the model output, output type, output shape is valid
     """
     @throw_ersilia_exception
     def check_information(self):
         self.logger.debug("Checking that model information is correct")
+        print(BOLD + "Beginning checks for {0} model information:".format(self.model_id) + RESET)
         json_file = os.path.join(self._dest_dir, self.model_id, INFORMATION_FILE)
         with open(json_file, "r") as f:
             data = json.load(f)
-        if data["card"]["Identifier"] != self.model_id:
-            raise texc.WrongCardIdentifierError(self.model_id)
-        return data
+
+        self._check_model_id(data)
+        self._check_model_slug(data)
+        self._check_model_description(data)
+        self._check_model_task(data)
+        self._check_model_input(data)
+        self._check_model_input_shape(data)
+        self._check_model_output(data)
+        self._check_model_output_type(data)
+        self._check_model_output_shape(data)
+        print("SUCCESS! Model information verified.\n")
+
 
     """
     Runs the model on a single smiles string and prints the output, or writes it to specified output file
@@ -74,7 +216,7 @@ class ModelTester(ErsiliaBase):
         session = Session(config_json=None)
         service_class = session.current_service_class()
 
-        click.echo("Testing model on single smiles input...\n")
+        click.echo(BOLD + "Testing model on single smiles input...\n" + RESET)
 
         input = "COc1ccc2c(NC(=O)Nc3cccc(C(F)(F)F)n3)ccnc2c1"
         mdl = ErsiliaModel(self.model_id, service_class=service_class, config_json=None)
@@ -87,7 +229,7 @@ class ModelTester(ErsiliaBase):
     """
     @throw_ersilia_exception
     def check_example_input(self, output):
-        click.echo("\nTesting model on input of 5 smiles given by 'example' command...\n")
+        click.echo(BOLD + "\nTesting model on input of 5 smiles given by 'example' command...\n" + RESET)
 
         session = Session(config_json=None)
         service_class = session.current_service_class()
@@ -108,7 +250,7 @@ class ModelTester(ErsiliaBase):
     @throw_ersilia_exception
     def check_consistent_output(self, output):
         # self.logger.debug("Confirming model produces consistent output...")
-        click.echo("\nConfirming model produces consistent output...")
+        click.echo(BOLD + "\nConfirming model produces consistent output..." + RESET)
 
         session = Session(config_json=None)
         service_class = session.current_service_class()
@@ -123,45 +265,102 @@ class ModelTester(ErsiliaBase):
         consistent = True
         zipped = list(zip(result, result2))
 
+
+        """
+        ***
+        We can only check for this 5% threshold if the outputs are numbers. If strings, it won't work 
+        ***
+        """
+
+
         for item1, item2 in zipped:
-            if (item1 != item2):
-                consistent = False
-                break
+            output1 = item1['output']['mw']
+            output2 = item2['output']['mw']
+            # check to see if the first and second outputs are within 5% from each other 
+            if (100 * (abs(output2 - output1) / ((output1 + output2) / 2)) > DIFFERENCE_THRESHOLD):
+                for item1, item2 in zipped:
+                    print(item1)
+                    print(item2)
+                    print('\n')
+                raise texc.InconsistentOutputs(self.model_id)
 
-        if consistent:
-            print("Model output is consistent!")
-        else: 
-            print("Model output is inconsistent. Please review the output shown below to see if there is an issue with the model. If the differences are small, there may be no issue!\n")
-            for item1, item2 in zipped:
-                print(item1)
-                print(item2)
-                print('\n')
-
-        click.echo("\nConfirming there are same number of outputs as inputs...")
+        print("Model output is consistent!")
+    
+        click.echo(BOLD + "\nConfirming there are same number of outputs as inputs..." + RESET)
         print("Number of inputs:", NUM_SAMPLES)
         print("Number of outputs:", len(zipped))
 
         if NUM_SAMPLES != len(zipped): 
-            raise texc.MissingOutputs(self.model_id)
+            raise texc.MissingOutputs()
         else: 
             echo("Number of outputs and inputs are equal!")
 
-
+    
     @throw_ersilia_exception
     def run_bash(self, output): 
-        pass
+        print("Running the model bash script...")
+        # Save current directory - atm, this must be run from root directory (~)
+        current_dir = os.getcwd()
 
+        # Create temp directory and clone model 
+        with tempfile.TemporaryDirectory() as temp_dir:
+            repo_url = 'https://github.com/ersilia-os/{0}.git'.format(self.model_id)  # Replace with the actual GitHub repository URL
+            try:
+                subprocess.run(['git', 'clone', repo_url, temp_dir], check=True)
+            except subprocess.CalledProcessError as e:
+                print(f"Error while cloning the repository: {e}")
+
+            # Navigate into the temporary directory
+            subdirectory_path = os.path.join(temp_dir, "model/framework")
+            os.chdir(subdirectory_path)
+
+            # Create temp file
+            with tempfile.NamedTemporaryFile(mode='w', delete=False) as temp_file:
+                temp_file_path = temp_file.name
+
+            # Run bash script with specified args
+            output_path = temp_file_path
+            run_path = os.path.join(temp_dir, "model/framework/run.sh")      # path to run.sh
+            arg1 = os.path.join(current_dir, "ersilia/test/inputs/compound_singles.csv")      # input
+            arg2 = output_path      # output
+
+            try:
+                subprocess.run(['bash', run_path, ".", arg1, arg2,], check=True)
+            except subprocess.CalledProcessError as e:
+                print(f"Error while running the bash script: {e}")
+
+            with open(output_path, 'r') as temp_file:
+                output_contents = temp_file.read()
+
+            print("Output contents:")
+            print(output_contents)
+
+            # maybe instead of printing the contents of the bash, we can run just compare it with an ersilia run
+
+
+    @throw_ersilia_exception
+    def run_using_bash(self): 
+        tmp_folder = tempfile.mkdtemp(prefix="eos-")
+        run_file = os.path.join(tmp_folder, self.RUN_FILE)
+        data_file = os.path.join(tmp_folder, DATA_FILE)
+        output_file = os.path.join(tmp_folder, OUTPUT_FILE)
+
+        cur_path = os.path.dirname(os.path.realpath(__file__))
+        framework_dir = os.path.join(cur_path, "..", "..", "..", self.model_id, "model", FRAMEWORK_BASEDIR, "run.sh")
+
+        subprocess.run(["chmod +x " + framework_dir, framework_dir, data_file, output_file], shell=True)
 
     def run(self, output):
         self.check_information()
         self.check_single_input(output)
         self.check_example_input(output)
         self.check_consistent_output(output)
+        # self.run_using_bash()
         # self.run_bash(output)
 
 
 # To do: 
-# 1. When it currently prints to an output file, it writes the single output result, then deletes that, then prints the result for the example input. Fix this
+# 1. When it currently prints to an output file, it writes the single output result, then deletes that, then prints the result for the example input. Fix this 
+    # to do this, write each output to a temporary file, and then append all of them to a final file and delete the temporary files at the end 
 # 2. test it with normal run and then try the bash run.sh, comparing the two outputs 
-# 3. speed it up by making it so the check_consistent_output function doesn't have to re-generate the example input and then run the model on that input twice
-# 4. for each major aspect of the test module, I'd like the header when showing the user what it's doing to be more like a header (bold, different color, etc.)
+# 3. Make sure the output matches the expectation of the otuput (ex: if it expects a float, it actually gets a float)
--- ersilia/utils/exceptions_utils/test_exceptions.py
+++ ersilia/utils/exceptions_utils/test_exceptions.py
@@ -3,33 +3,41 @@ from .exceptions import ErsiliaError
 
 class WrongCardIdentifierError(ErsiliaError):
     def __init__(self, model_id):
-        self.message = (
-            "The model identifier in the model card is not correct: {0}".format(
-                model_id
-            )
-        )
-        self.hints = (
-            "Check the model information, usually available in a metadata.json file."
-        )
+        self.message = ("The model identifier in the model card is not correct: {0}".format(model_id))
+        self.hints = ("Check the model information, usually available in a metadata.json file.")
         super().__init__(self.message, self.hints)
 
 
 class InformationFileNotExist(ErsiliaError): 
      def __init__(self, model_id):
-        self.message = (
-            "The eos/dest/{0}/information.json file does not exist.".format(
-                model_id
-            )
-        )
-        self.hints = (
-            "Try fetching and serving the model first."
-        )
+        self.message = ("The eos/dest/{0}/information.json file does not exist.".format(model_id))
+        self.hints = ("Try fetching and serving the model first, and make sure the model is written correctly.")
         super().__init__(self.message, self.hints)
 
 
 class MissingOutputs(ErsiliaError): 
-    def __init__(self, model_id):
+    def __init__(self):
         self.message = ("There are not as many outputs as there are inputs. They must be the same.\n")
-        self.hints = ("Check whether or not the code for the model is skipping a header, or if any of the smiles used are not working correctly.")
+        self.hints = ("Try checking whether or not the code for the model is skipping a header, or if any of the smiles used are not working correctly.")
+        super().__init__(self.message, self.hints)
+
+
+class InconsistentOutputs(ErsiliaError): 
+    def __init__(self, model_id):
+        self.message = ("Model outputs are inconsistent, meaning the outputs do not fit the 5% similarity threshold.")
+        self.hints = ("Observe the output comparisons for each smiles input above.")
+        super().__init__(self.message, self.hints)
+
+
+class EmptyField(ErsiliaError):
+    def __init__(self, empty_field):
+        self.message = ("The {0} field in the model card is empty.".format(empty_field))
+        self.hints = ("Check the model information, usually available in a metadata.json file.")
+        super().__init__(self.message, self.hints)
+
+class InvalidEntry(ErsiliaError):
+    def __init__(self, invalid_field):
+        self.message = ("The {0} field of this model is not recognized as a valid format.".format(invalid_field))
+        self.hints = ("Check the model information, usually available in a metadata.json file.")
         super().__init__(self.message, self.hints)
 
