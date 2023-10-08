a8d754d8ceddde2727a4dc31a0984dc3a31fe640
Anthony Cui <acui1@college.harvard.edu>
2023-10-08 13:36:49 -0400

    Test bad code formatting

--- ersilia/core/tracking.py
+++ ersilia/core/tracking.py
@@ -5,7 +5,6 @@ class RunTracker:
 
     NOTE: Currently, the Splunk connection is not set up. For now, we will print tracking results to the console.
     """
-
     def track(self, input, result, meta):
         """
         Tracks the results after a model run.
