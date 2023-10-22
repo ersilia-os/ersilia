d6ad7607024ac08a16742aa60a5df5ddb062d4f5
Theresa <thuang@college.harvard.edu>
2023-10-22 16:59:31 -0400

    Write outputs to JSON and upload to S3 Bucket

--- ersilia/core/tracking.py
+++ ersilia/core/tracking.py
@@ -1,7 +1,12 @@
 from datetime import datetime
 import json
 import pandas as pd
-
+import tracemalloc
+# from ersilia import cli
+import logging
+import boto3
+from botocore.exceptions import ClientError
+import os
 
 class RunTracker:
     """
@@ -48,3 +53,42 @@ class RunTracker:
         data = json.load(result)
         self.log_to_console(result)
         return data
+    
+    def start(self):
+        tracemalloc.start()
+        self.time_start = tracemalloc.get_traced_memory()[0]
+    
+    def track_memory(self):
+        peak_memory = tracemalloc.get_traced_memory()[1] - self.time_start
+        print(f"Peak memory: {peak_memory}")
+        tracemalloc.stop()
+
+
+def write_file(dict):
+    str = json.dump(dict)
+    tmp = tempfile.NamedTemporaryFile()
+
+    with open(tmp.name, 'w') as f:
+        f.write(str)
+
+def upload_file(file_name, bucket, object_name=None):
+    """Upload a file to an S3 bucket
+
+    :param file_name: File to upload
+    :param bucket: Bucket to upload to
+    :param object_name: S3 object name. If not specified then file_name is used
+    :return: True if file was uploaded, else False
+    """
+
+    # If S3 object_name was not specified, use file_name
+    if object_name is None:
+        object_name = datetime.now().strftime("%Y-%m-%d-%H-%M-%S") + '-' + os.path.basename(file_name)
+
+    # Upload the file
+    s3_client = boto3.client('s3')
+    try:
+        response = s3_client.upload_file(file_name, bucket, object_name)
+    except ClientError as e:
+        logging.error(e)
+        return False
+    return True
