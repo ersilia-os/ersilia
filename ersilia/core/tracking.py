1c1f97eb1acf38f904f2a20e11b5d8b13ebc4084
Anthony Cui <acui1@college.harvard.edu>
2023-10-27 23:18:38 -0400

    Update tracking to include all metrics

--- ersilia/core/tracking.py
+++ ersilia/core/tracking.py
@@ -36,15 +36,21 @@ class RunTracker:
         dat = dat.drop(["key", "input"], axis=1)
 
         # calculate and print statistics
+        stats = {}
         for column in dat:
-            print("Mean %s: %s" % (column, dat[column].mean()))
+            column_stats = {}
+            column_stats['mean'] = dat[column].mean()
             if len(dat[column].mode()) == 1:
-                print("Mode %s: %s" % (column, dat[column].mode()))
+                column_stats['mode'] = dat[column].mode()
             else:
-                print("No mode")
-            print("Min %s: %s" % (column, dat[column].min()))
-            print("Max %s: %s" % (column, dat[column].max()))
-            print("Standard deviation %s: %s" % (column, dat[column].std()))
+                column_stats['mode'] = None
+            column_stats['min'] = dat[column].min()
+            column_stats['max'] = dat[column].max()
+            column_stats['std'] = dat[column].std()
+
+            stats[column] = column_stats
+
+        return stats
 
     def get_file_sizes(self, input_df, output_df):
         input_size = input_df.memory_usage(deep=True).sum() / 1024
@@ -53,8 +59,12 @@ class RunTracker:
         input_avg_row_size = input_size / len(input_df)
         output_avg_row_size = output_size / len(output_df)
 
-        print("Average Input Row Size (KB):", input_avg_row_size)
-        print("Average Output Row Size (KB):", output_avg_row_size)
+        return {
+            "input_size": input_size,
+            "output_size": output_size,
+            "avg_input_size": input_avg_row_size,
+            "avg_output_size": output_avg_row_size,
+        }
 
     def track(self, input, result, meta):
         """
@@ -64,37 +74,26 @@ class RunTracker:
         input_dataframe = read_csv(input)
         result_dataframe = read_csv(result)
 
-        print("Run input file:", input)
-        print(input_dataframe)
-        json_dict["input"] = input
         json_dict["input_dataframe"] = input_dataframe.to_dict()
-
-        print("Run output file:", result)
-        print(result_dataframe)
-        json_dict["result"] = result
         json_dict["result_dataframe"] = result_dataframe.to_dict()
 
-        print("Model metadata:", meta)
         json_dict["meta"] = meta
 
         model_id = meta["metadata"].get("Identifier", "Unknown")
-        print("Model ID:", model_id)
         json_dict["model_id"] = model_id
 
         time = datetime.now() - self.time_start
-        print("Time taken:", time)
         json_dict["time_taken"] = str(time)
 
         # checking for mismatched types
         nan_count = result_dataframe.isna().sum()
-        print("\nNAN Count:\n", nan_count)
         json_dict["nan_count"] = nan_count.to_dict()
 
-        self.check_types(result_dataframe, meta["metadata"])
+        json_dict["check_types"] = self.check_types(result_dataframe, meta["metadata"])
 
-        self.stats(result)
+        json_dict["stats"] = self.stats(result)
 
-        self.get_file_sizes(input_dataframe, result_dataframe)
+        json_dict['file_sizes'] = self.get_file_sizes(input_dataframe, result_dataframe)
 
         json_object = json.dumps(json_dict, indent=4)
         print("\nJSON Dictionary:\n", json_object)
@@ -102,12 +101,7 @@ class RunTracker:
         # log results to console
         with open("../cli/commands/current_session.txt", "a") as f:
             # write the print statements to a file
-            f.write(f"\n{json.dumps(input_dataframe)}\n")
-            f.write(f"\n{json.dumps(result_dataframe)}\n")
-            f.write(f"\n{json.dumps(meta)}\n")
-            f.write(f"\nModel ID: {model_id}\n")
-            f.write(f"\nTime taken: {time}\n")
-            f.write(f"\nNAN Count:\n {nan_count}\n")
+            f.write(json_object)
 
     def log_to_console(self, data):
         print(f"\n{json.dumps(data)}\n")
@@ -130,9 +124,17 @@ class RunTracker:
 
         if len(dtypesLst) > 1 and metadata["Output Shape"] != "List":
             print("Not right shape. Expected List but got Single")
+            correct_shape = False
         elif len(dtypesLst) == 1 and metadata["Output Shape"] != "Single":
             print("Not right shape. Expected Single but got List")
+            correct_shape = False
         else:
             print("Output is correct shape.")
+            correct_shape = True
 
         print("Output has", count, "mismatched types.\n")
+
+        return {
+            "mismatched_types": count,
+            "correct_shape": correct_shape
+        }
