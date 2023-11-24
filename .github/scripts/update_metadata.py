0e9e99d19af3acdda1651637164474e6eb25e7e3
gemmaturon <gemma@ersilia.io>
2023-11-24 11:07:55 +0100

    Update update_metadata.py

--- .github/scripts/update_metadata.py
+++ .github/scripts/update_metadata.py
@@ -113,9 +113,7 @@ class UpdateMetadata:
         if self.metadata["Tag"] == []:
             # split the tags into a list andremove any whitespace
             # tags = [tag.strip() for tag in self.json_input["tag"].split(",")]
-            print(type(self.json_input["tag"]))
-            tags = self.json_input["tag"]
-            self.metadata["Tag"] = tags
+            self.metadata["Tag"] = []
         if self.metadata["Status"] == "":
             self.metadata["Status"] = "In progress"
 
