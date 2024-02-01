03a1306351be712fba20dca13486d3244f4f71fe
Carolina Caballero López <carolina.cl19@gmail.com>
2024-01-31 20:30:48 -0500

    fix issue of enviroment

--- .github/scripts/convert_airtable_to_csv.py
+++ .github/scripts/convert_airtable_to_csv.py
@@ -1,19 +1,21 @@
-import sys
 import os
 from airtable import Airtable
 import csv
 
+airtable_api_key = os.environ.get('AIRTABLE_API_KEY')
+airtable_base_id = os.environ.get('AIRTABLE_BASE_ID')
+airtable_table_id = os.environ.get('AIRTABLE_TABLE_NAME')
 
-def convert_airtable_to_csv(airtable_api_key, airtable_base_id,airtable_table_id,output_path):
+def convert_airtable_to_csv(airtable_api_key, airtable_base_id,airtable_table_id):
 
     base = Airtable(airtable_base_id, airtable_table_id, airtable_api_key)
     records = base.get_all()
 
     script_directory = os.path.dirname(os.path.abspath(__file__))
-    full_output_path = os.path.join(script_directory, "../../hub/content/data/")
+    full_output_path = os.path.join(script_directory, "../../hub/content/data/models_from_airtable.csv")
     os.makedirs(os.path.dirname(full_output_path), exist_ok=True)
 
-    csv_file = os.path.join(full_output_path, output_path)
+    csv_file = os.path.join(full_output_path)
 
     with open(csv_file, 'w') as f:
         writer = csv.writer(f)
@@ -22,9 +24,9 @@ def convert_airtable_to_csv(airtable_api_key, airtable_base_id,airtable_table_id
             writer.writerow(record["fields"].values())
     
 if __name__ == "__main__":
-    airtable_api_key = sys.argv[1]
-    airtable_base_id= sys.argv[2]
-    airtable_table_id= sys.argv[3]
-    output_path = sys.argv[4]
 
-    convert_airtable_to_csv(airtable_api_key, airtable_base_id,airtable_table_id,output_path)
+    airtable_api_key = os.environ.get('AIRTABLE_API_KEY')
+    airtable_base_id = os.environ.get('AIRTABLE_BASE_ID')
+    airtable_table_id = os.environ.get('AIRTABLE_TABLE_NAME')
+
+    convert_airtable_to_csv(airtable_api_key, airtable_base_id,airtable_table_id)
