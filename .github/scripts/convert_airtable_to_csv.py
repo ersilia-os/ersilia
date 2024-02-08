48bd32488431f269f284c36e4b9b86e7af700a00
Carolina Caballero López <carolina.cl19@gmail.com>
2024-02-08 08:41:56 -0500

    improve files

--- .github/scripts/convert_airtable_to_csv.py
+++ .github/scripts/convert_airtable_to_csv.py
@@ -2,22 +2,17 @@ import os
 from airtable import Airtable
 import csv
 
-airtable_api_key = os.environ.get('AIRTABLE_API_KEY')
-airtable_base_id = os.environ.get('AIRTABLE_BASE_ID')
-airtable_table_id = os.environ.get('AIRTABLE_TABLE_NAME')
+current_directory = os.path.dirname(os.path.abspath(__file__))
 
-def convert_airtable_to_csv(airtable_api_key, airtable_base_id,airtable_table_id):
+data_directory = os.path.join(current_directory, '../../ersilia/hub/content/data')
+os.makedirs(data_directory, exist_ok=True)
+file_path= os.path.join(data_directory, 'models.csv')
 
+def convert_airtable_to_csv(airtable_api_key, airtable_base_id,airtable_table_id,file_path):
+ 
     base = Airtable(airtable_base_id, airtable_table_id, airtable_api_key)
     records = base.get_all()
-
-    script_directory = os.path.dirname(os.path.abspath(__file__))
-    full_output_path = os.path.join(script_directory, "../../hub/content/data/models_from_airtable.csv")
-    os.makedirs(os.path.dirname(full_output_path), exist_ok=True)
-
-    csv_file = os.path.join(full_output_path)
-
-    with open(csv_file, 'w') as f:
+    with open(file_path, 'w') as f:
         writer = csv.writer(f)
         writer.writerow(records[0]["fields"].keys())
         for record in records:
@@ -29,4 +24,4 @@ if __name__ == "__main__":
     airtable_base_id = os.environ.get('AIRTABLE_BASE_ID')
     airtable_table_id = os.environ.get('AIRTABLE_TABLE_NAME')
 
-    convert_airtable_to_csv(airtable_api_key, airtable_base_id,airtable_table_id)
+    convert_airtable_to_csv(airtable_api_key, airtable_base_id,airtable_table_id, file_path)
