9ec6e862006bd3436535b835512d491a8adf8ad6
Miquel Duran-Frigola <miquelduranfrigola@gmail.com>
2024-02-08 15:47:45 +0100

    Update convert_airtable_to_csv.py

--- .github/scripts/convert_airtable_to_csv.py
+++ .github/scripts/convert_airtable_to_csv.py
@@ -6,15 +6,15 @@ current_directory = os.path.dirname(os.path.abspath(__file__))
 
 data_directory = os.path.join(current_directory, '../../ersilia/hub/content/data')
 os.makedirs(data_directory, exist_ok=True)
-file_path= os.path.abspath(os.path.join(data_directory, 'models.csv'))
+file_path= os.path.abspath(os.path.join(data_directory, 'models.tsv'))
 
-def convert_airtable_to_csv(airtable_api_key, airtable_base_id,airtable_table_id,file_path):
+def convert_airtable_to_tsv(airtable_api_key, airtable_base_id,airtable_table_id,file_path):
  
     base = Airtable(airtable_base_id, airtable_table_id, airtable_api_key)
     records = base.get_all()
     with open(file_path, 'w') as f:
         print("Writing to {0}".format(file_path))
-        writer = csv.writer(f)
+        writer = csv.writer(f, delimiter="\t")
         writer.writerow(records[0]["fields"].keys())
         for record in records:
             writer.writerow(record["fields"].values())
@@ -27,4 +27,4 @@ if __name__ == "__main__":
     airtable_table_id = os.environ.get('AIRTABLE_TABLE_NAME')
 
     print("Converting AirTable base to CSV file")
-    convert_airtable_to_csv(airtable_api_key, airtable_base_id,airtable_table_id, file_path)
+    convert_airtable_to_tsv(airtable_api_key, airtable_base_id,airtable_table_id, file_path)
