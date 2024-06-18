05e35d993f32ea0d34681381156600d66f8ffdc3
Malik Badmus <malik.badmus@icloud.com>
2024-06-18 11:45:36 +0100

    Implement the log_file_metrics method

--- ersilia/core/tracking.py
+++ ersilia/core/tracking.py
@@ -23,9 +23,6 @@ from ..io.output_logger import TabularResultLogger
 from botocore.exceptions import ClientError, NoCredentialsError
 
 
-# Temporary path to log files until log files are fixed
-TEMP_FILE_LOGS = os.path.abspath("")
-
 
 def docker_stats(container_name=None):
     """
@@ -85,7 +82,7 @@ def docker_stats(container_name=None):
         return [f"An unexpected error occurred: {e}"]
 
 
-def log_files_metrics(file, model_id):
+def log_files_metrics(file_log, model_id):
     """
     This function will log the number of errors and warnings in the log files.
 
@@ -100,17 +97,19 @@ def log_files_metrics(file, model_id):
     misc_error_flag = False
     error_name = ""
     errors = {}
+    json_dict = {}
 
     try:
-        with open(file, "r") as file:
+        with open(file_log, "r") as file:
             line = None
             for line in file:
                 if not re.match(r"^\d{2}.\d{2}.\d{2} \| ", line):
-                    # continuation of log
                     if ersilia_error_flag:
-                        # catch the error name if hinted by previous line
                         error_name = line.rstrip()
-                        errors[error_name] += 1
+                        if error_name in errors:
+                            errors[error_name] += 1
+                        else:
+                            errors[error_name] = 1
                         ersilia_error_flag = False
                         continue
                     elif misc_error_flag:
@@ -122,10 +121,10 @@ def log_files_metrics(file, model_id):
                     # encountering new logs
                     # make sure error flags are closed
                     if ersilia_error_flag:
-                        errors["Unknown Ersilia exception class"] += 1
+                        errors["Unknown Ersilia exception class"] = errors.get("Unknown Ersilia exception class", 0) + 1
                         ersilia_error_flag = False
                     if misc_error_flag:
-                        errors[error_name] += 1
+                        errors[error_name] = errors.get(error_name, 0) + 1
                         misc_error_flag = False
                     if "| ERROR" in line:
                         error_count += 1
@@ -147,13 +146,17 @@ def log_files_metrics(file, model_id):
                     errors["Unknown Ersilia exception class"] += 1
                 if misc_error_flag:
                     errors[error_name] += 1
-
-        write_persistent_file(f"Error count: {error_count}", model_id)
+        
+        json_dict = {}
+        json_dict["Error count"] = error_count
+        
         if len(errors) > 0:
-            write_persistent_file(f"Breakdown by error types:", model_id)
+            json_dict["Breakdown by error types"] = {}
             for error in errors:
-                write_persistent_file(f"{error}: {errors[error]}", model_id)
-        write_persistent_file(f"Warning count: {warning_count}", model_id)
+                json_dict["Breakdown by error types"][error] = errors[error]      
+        json_dict["Warning count"] = warning_count      
+        json_object = json.dumps(json_dict, indent=4)
+        write_persistent_file(json_object, model_id)
     except (IsADirectoryError, FileNotFoundError):
         logging.warning("Unable to calculate metrics for log file: log file not found")
 
@@ -217,7 +220,10 @@ def close_persistent_file(model_id):
     """
     if check_file_exists(model_id):
         file_name = get_persistent_file_path(model_id)
-        log_files_metrics(TEMP_FILE_LOGS, model_id)
+        file_log = os.path.join(
+        EOS,  "console.log"
+    )
+        log_files_metrics(file_log, model_id)
 
         new_file_path = os.path.join(
             os.path.dirname(file_name),
@@ -229,8 +235,9 @@ def close_persistent_file(model_id):
         raise FileNotFoundError(
             f"The persistent file for model {model_id} does not exist. Cannot close file."
         )
-
-
+        
+        
+        
 def upload_to_s3(json_dict, bucket="ersilia-tracking", object_name=None):
     """Upload a file to an S3 bucket
 
@@ -547,6 +554,8 @@ class RunTracker(ErsiliaBase):
             return "No such process found."
         except Exception as e:
             return str(e)
+            
+            
 
     def log_result(self, result):
         output_dir = os.path.join(self.lake_folder, self.model_id)
@@ -576,7 +585,9 @@ class RunTracker(ErsiliaBase):
         file_name = os.path.join(output_dir, "{0}.log".format(self.model_id))
         session_file = os.path.join(EOS, "session.json")
         shutil.copyfile(session_file, file_name)
-
+        
+        
+        
     def track(self, input, result, meta):
         """
         Tracks the results of a model run.
