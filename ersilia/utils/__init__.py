0033b125b0acde41c326607ea3d1920e94f7df9d
dhanshreea <DhanshreeA@users.noreply.github.com>
2024-07-17 17:01:30 +0200

    Move the currently served model's pid to its dedicated session directory

--- ersilia/utils/__init__.py
+++ ersilia/utils/__init__.py
@@ -1,7 +1,9 @@
 import os
-from ..default import EOS
+from ..default import SESSIONS_DIR
+from ..utils.session import get_session_id
 
 
 def tmp_pid_file(model_id):
     """Gets filename to store process ID"""
-    return os.path.join(EOS, "tmp", "{0}.pid".format(model_id))
+    session_dir = os.path.join(SESSIONS_DIR, get_session_id())
+    return os.path.join(session_dir, "{0}.pid".format(model_id))
