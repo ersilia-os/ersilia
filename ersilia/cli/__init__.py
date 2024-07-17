23aa254d88d37c2dd9322021736ae3313580aae7
dhanshreea <DhanshreeA@users.noreply.github.com>
2024-07-17 16:35:48 +0200

    Create a session at ersilia cli initialization

--- ersilia/cli/__init__.py
+++ ersilia/cli/__init__.py
@@ -1,7 +1,8 @@
 from .create_cli import create_ersilia_cli
 from .echo import echo
+from ..utils.session import create_session_dir
 
 cli = create_ersilia_cli()
-
+create_session_dir()
 if __name__ == "__main__":
     cli()
