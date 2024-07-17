280383d3315e4014dd2eb5e74085b6d076bdf390
dhanshreea <DhanshreeA@users.noreply.github.com>
2024-07-17 14:18:59 +0200

    WIP Add session management utils

---
+++ ersilia/utils/session.py
@@ -0,0 +1,44 @@
+import os
+import shutil
+import psutil
+
+from ..default import SESSIONS_DIR
+
+def get_current_pid():
+    return os.getpid()
+
+def get_parent_pid():
+    pid = os.getppid()
+    return pid
+
+def create_session_files(session_name):
+    session_dir = os.path.join(SESSIONS_DIR, session_name)
+    os.makedirs(os.path.join(session_dir, "logs"), exist_ok=True)
+
+def create_session_dir():
+    remove_orphaned_sessions()
+    session_name = f"session_{get_parent_pid()}"
+    session_dir = os.path.join(SESSIONS_DIR, session_name)
+    os.makedirs(session_dir, exist_ok=True)
+    create_session_files(session_name)
+
+def remove_session_dir(session_name):
+    session_dir = os.path.join(SESSIONS_DIR, session_name)
+    shutil.rmtree(session_dir)
+
+def determine_orphaned_session():
+    # TODO maybe this is slow, look out for performance
+    _sessions = []
+    for session in os.listdir(SESSIONS_DIR):
+        session_pid = int(session.split("_")[1])
+        if not psutil.pid_exists(session_pid):
+            _sessions.append(session)
+    return _sessions
+
+def remove_orphaned_sessions():
+    orphaned_sessions = determine_orphaned_session()
+    for session in orphaned_sessions:
+        remove_session_dir(session)
+
+def get_session_id():
+    return f"session_{get_parent_pid()}"
