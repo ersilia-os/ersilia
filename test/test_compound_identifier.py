8e1bebac6e2ba341c7b265bd7f33e6a3b9ad8420
musasizivictoria <musasizivicky06@gmail.com>
2024-10-15 12:12:45 +0300

    proper error reporting

--- test/test_compound_identifier.py
+++ test/test_compound_identifier.py
@@ -5,8 +5,7 @@ from ersilia.utils.identifiers.compound import CompoundIdentifier
 def compound_identifier():
     return CompoundIdentifier()
 
-def test_is_input_header_positive(compound_identifier):
+@pytest.mark.parametrize("header", ["smiles", "input", "SMILES", "INPUT"])
+def test_is_input_header_positive(compound_identifier, header):
     """Test that valid input headers return True."""
-    valid_headers = ["smiles", "input", "SMILES", "INPUT"]
-    for header in valid_headers:
-        assert compound_identifier.is_input_header(header) is True
+    assert compound_identifier.is_input_header(header) is True
