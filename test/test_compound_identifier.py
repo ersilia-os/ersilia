ac35f56432b373c4ce45a12af8d0b63265618f67
Ajoke Yusuf <ajokeyusuf10@gmail.com>
2024-10-17 15:22:01 +0100

    Create is_key_header negative test for test_compound_identifier.py

--- test/test_compound_identifier.py
+++ test/test_compound_identifier.py
@@ -9,3 +9,7 @@ def compound_identifier():
 def test_is_input_header_positive(compound_identifier, header):
     """Test that valid input headers return True."""
     assert compound_identifier.is_input_header(header) is True
+
+@pytest.mark.parametrize("header", ["id","smiles","inchi","input", "some_header", "random", "header", ""])
+def test_is_key_header_negative(compound_identifier, header):
+    assert not compound_identifier.is_key_header(header)
