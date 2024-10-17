76725d78a8491e33c2a2f285fb7c77d9979a08d3
musasizivictoria <musasizivicky06@gmail.com>
2024-10-17 17:24:27 +0300

    add key header tests

--- test/test_compound_identifier.py
+++ test/test_compound_identifier.py
@@ -14,3 +14,13 @@ def test_is_input_header_positive(compound_identifier, header):
 def test_is_input_header_negative(compound_identifier, header):
     """Test that invalid input headers return False."""
     assert compound_identifier.is_input_header(header) is False
+
+@pytest.mark.parametrize("header", ["inchikey", "key", "INCHIKEY", "KEY"])
+def test_is_key_header_positive(compound_identifier, header):
+    """Test that valid key headers return True."""
+    assert compound_identifier.is_key_header(header) is True
+
+@pytest.mark.parametrize("header", ["smiles", "input", "invalid", "123","someHeader"])
+def test_is_key_header_negative(compound_identifier, header):
+    """Test that invalid key headers return False."""
+    assert compound_identifier.is_key_header(header) is False
