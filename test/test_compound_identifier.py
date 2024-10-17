4807cb963ea5637c66f2f87b090f5154396f44f5
mercybassey <udohmercy911@gmail.com>
2024-10-17 15:01:08 +0100

    integreted parameterization

--- test/test_compound_identifier.py
+++ test/test_compound_identifier.py
@@ -9,3 +9,8 @@ def compound_identifier():
 def test_is_input_header_positive(compound_identifier, header):
     """Test that valid input headers return True."""
     assert compound_identifier.is_input_header(header) is True
+
+@pytest.mark.parametrize("header", ["key", "inchiKey", "KEY", "INCHIKEY"])
+def test_is_key_header_positive(compound_identifier, header):
+    """Test that valid key headers return True."""
+    assert compound_identifier.is_key_header(header) is True
