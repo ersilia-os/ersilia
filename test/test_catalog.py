745b6c3f858db1fb61955a8f2bb534c1e3f55c6c
Farida Momoh <95109808+KimFarida@users.noreply.github.com>
2024-10-18 11:19:35 +0100

    feat: add pytest fixture to load catalog samples from JSON file

---
+++ test/test_catalog.py
@@ -0,0 +1,17 @@
+import json
+import os
+import pytest
+from ersilia.hub.content.catalog import CatalogTable
+
+
+@pytest.fixture
+def catalog_samples():
+    """
+    Fixture that loads test data from a JSON file.
+    The JSON file contains realtime catalog samples for testing
+    """
+    file_path = os.path.join(os.path.dirname(__file__), 'inputs', 'test_catalog','catalog_sample.json')
+    with open(file_path, 'r') as f:
+        samples = json.load(f)
+    return samples
+
