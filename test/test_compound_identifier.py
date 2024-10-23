5aa8fa7c3a6c36859c11ba439f6ded25a8c0090f
musasizivictoria <musasizivicky06@gmail.com>
2024-10-23 11:48:08 +0300

    replace removed test

--- test/test_compound_identifier.py
+++ test/test_compound_identifier.py
@@ -14,3 +14,8 @@ def test_is_input_header_positive(compound_identifier, header):
 def test_is_input_header_negative(compound_identifier, header):
     """Test that invalid input headers return False."""
     assert compound_identifier.is_input_header(header) is False
+
+@pytest.mark.parametrize("header", ["key", "inchiKey", "KEY", "INCHIKEY"])
+def test_is_key_header_positive(compound_identifier, header):
+    """Test that valid key headers return True."""
+    assert compound_identifier.is_key_header(header) is True
