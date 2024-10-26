543b52272bea270a82dd813e33809e42dfab3f80
Miquel Duran-Frigola <miquelduranfrigola@gmail.com>
2024-10-26 17:01:54 +0200

    scripts to process model metadata and publications

--- .github/scripts/suggest_metadata_with_llm.py
+++ .github/scripts/suggest_metadata_with_llm.py
@@ -1,18 +1,50 @@
 import os
+import argparse
+import openai
 import requests
+import boto3
+import tempfile
+import shutil
 from dotenv import load_dotenv
 
 load_dotenv()
-
 OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
-AWS_ACCESS_KEY_ID = os.getenv("AWS_ACCESS")
+openai.api_key = OPENAI_API_KEY
+MODEL_NAME = "gpt-4o"
+
+
+def read_accepted_labels(category):
+    category = category.lower().replace(" ", "_")
+    base_url = "https://raw.githubusercontent.com/ersilia-os/ersilia/refs/heads/master/ersilia/hub/content/metadata/"
+    url = os.path.join(base_url, category + ".txt")
+    response = requests.get(url)
+    if response.status_code == 200:
+        values = response.text.splitlines()
+        return [x.strip() for x in values if x]
+    else:
+        raise Exception(f"Failed to download labels from {url}")
+
+
+accepted = {}
+for category in [
+    "input",
+    "input_shape",
+    "license",
+    "mode",
+    "output",
+    "output_shape",
+    "output_type",
+    "tag",
+    "task",
+]:
+    accepted[category] = ", ".join(read_accepted_labels(category))
 
 
 PRIMARY_SYSTEM_PROMPT = """
 You are a biomedical expert and you are asked to annotate metadata for a given computational tool, for example an AI/ML model.
 You will be given the following information:
-1. A structured report (summary) of a publication. This will be provided as an attachment.
-2. Some raw and potentially spurious metadata copied inline as part of the user prompt.
+1. A structured report (summary) of a publication. This will be labelled by the user as PUBLICATION REPORT.
+2. Some raw and potentially spurious metadata in JSON, YAML or Markdown format. This will be labeled by the user as RAW METADATA.
 
 Your task is to provide a structured metadata report for the computational tool based on the information provided.
 The metadata report should include the following information, in Markdown format:
@@ -26,17 +58,30 @@ Write a short summary of the computational tool. This should be a high-level ove
 You an also get inspiration from the user prompt.
 The description should have at least 200 characters. Strictly one paragraph.
 
+# Tags
+Select 1-5 tags from this list: {7}
+Give the tags as a list:
+- Tag 1
+- Tag 2
+- etc.
+
 # Input
-- Input type:
-- Input format:
+- Input: One of the following, depending on the type of input accepted for the model: {0}
+- Input shape: One of the following, depending on the characteristics of the minimum model input. Typically, models accept a single input. Models that require multiple inputs to do predictions, can be lists, etc.: {1}
 
 # Output
-- Output type:
-- Output format:
+- Output: One (ideally) or multiple of the following (comma-separated), depending on the type of output produced by the model: {2}
+- Output shape: One of the following, depending on the dimensionality of the produced output: {3}
+- Output type: One of the following: {4}
 
+# Mode and Task
+- Mode: One of the following, depending on whether the model is already pre-trained by authors, retrained by our team, or build from the data with our own tools: {5}
+- Task: AI, ML or data science task. One of the following: {6}
+
+# Interpretation
+Provide a oneliner explaining how to interpret the output of the model. Is it a probability? Is it a regression value for a particular experimental assay? etc. No more than 150 characters.
 
 # Publication and Code
-
 Provide a brief summary of the publication. You can use the TLDR from the publication report and the publication summary.
 This should be a high-level overview of the publication. Between 50 and 100 words. Only one paragraph. No new-line characters.
 
@@ -44,28 +89,306 @@ In addition, provide the following URLs as a list:
 - Publication URL: Provide the URL of the publication. This is exactly the URL provided in the metadata from the user.
 - Code URL: Provide the URL of the code repository. This is exactly the URL provided in the metadata from the user.
 
-"""
+# License
+- Simply extract the license from the metadata.
+
+--
+
+Below are some general guidelines:
+- Produce a Markdown file strictly following the headers and formatting specified above.
+- Never do multiple paragraphs.
+- Be concise.
+- Do not include any special characters, boldface, or italics.
+""".format(
+    accepted["input"],
+    accepted["input_shape"],
+    accepted["output"],
+    accepted["output_shape"],
+    accepted["output_type"],
+    accepted["mode"],
+    accepted["task"],
+    accepted["tag"],
+)
+
 
 PRIMARY_USER_PROMPT = """
+Write the metadata of the following computational tool.
 
+RAW METADATA
+<RAW_METADATA>
 
+PUBLICATION REPORT
+<PUBLICATION_REPORT>
+"""
+
+POSTPROCESS_SYSTEM_PROMPT = """
+Your task is to make sure the format of a Markdown file is correct. You can slightly modify the content, but only if you find
+inconsistencies in the text, repetitions, or incoherences. Avoid boldface, italics and special characters.
+Strictly follow the format below:
+# General information
+- Title: The title. No longer than 100 characters.
+- Slug: Lower case, can use hyphenation.
+
+# Description
+At least 200 characters, ideally more. Strictly one paragraph.
+
+# Tags
+- Tag 1
+- Tag 2
+- etc. 3 to 5 tags.
+
+# Input
+- Input: As specified in the source file. Do not modify.
+- Input shape: As specified in the source file. Do not modify.
+
+# Output
+- Output: As specified in the source file. Do not modify.
+- Output shape: As specified in the source file. Do not modify.
+- Output type: As specified in the source file. Do not modify.
+
+# Mode and Task
+- Mode: As specified in the source file. Do not modify.
+- Task: As specified in the source file. Do not modify.
+
+# Interpretation
+A one-liner. No more than 150 characters. Strictly one paragraph.
+
+# Publication and Code
+A summary as specified in the source file, between 50 and 100 words. Strictly one paragraph.
+
+- Publication: A URL with the link []()
+- Code: A URL with the link []()
+
+# License
+- As specified in the source file. Do not modify.
+"""
+
+POSTPROCESS_USER_PROMPT = """
+Process the following text:
 """
 
 
-
-class ContentFetcher(object):
-    def __init__(self, model_id):
+class MetadataDownloader(object):
+    def __init__(self, model_id=None, issue_number=None, url=None, file_path=None):
+        if (
+            model_id is None
+            and issue_number is None
+            and url is None
+            and file_path is None
+        ):
+            raise Exception("At least one argument is necessary")
+        if model_id is not None:
+            if issue_number is not None or url is not None or file_path is not None:
+                raise Exception("Only one argument is accepted")
+        if issue_number is not None:
+            if model_id is not None or url is not None or file_path is not None:
+                raise Exception("Only one argument is accepted")
+        if url is not None:
+            if (
+                model_id is not None
+                or issue_number is not None
+                or file_path is not None
+            ):
+                raise Exception("Only one argument is accepted")
+        if file_path is not None:
+            if model_id is not None or issue_number is not None or url is not None:
+                raise Exception("Only one argument is accepted")
         self.model_id = model_id
+        self.issue_number = issue_number
+        self.url = url
+        self.file_path = file_path
+        self.tmp_folder = tempfile.mkdtemp(prefix="ersilia-")
+        self.output_file = os.path.join(self.tmp_folder, "metadata.txt")
 
-    def get_publication_summary(self):
+    def download_by_model_id(self):
+        base_url = (
+            "https://raw.githubusercontent.com/ersilia-os/{0}/refs/heads/main/".format(
+                self.model_id
+            )
+        )
+        metadata_yml = os.path.join(base_url, "metadata.yml")
+        metadata_json = os.path.join(base_url, "metadata.json")
+        try:
+            url = os.path.join(base_url, metadata_yml)
+            response = requests.get(url)
+            if response.status_code == 200:
+                with open(self.output_file, "wb") as f:
+                    f.write(response.content)
+                return self.output_file
+        except:
+            pass
+        try:
+            url = os.path.join(base_url, metadata_json)
+            if response.status_code == 200:
+                with open(self.output_file, "wb") as f:
+                    f.write(response.content)
+                return self.output_file
+        except:
+            raise Exception("Metadata not found in model repository")
+
+    def download_by_issue_number(self):
+        # TODO extract metadata from the issue itself
         pass
 
-    def get_metadata_from_issue(self):
-        pass
+    def download_from_url(self):
+        response = requests.get(self.url)
+        if response.status_code == 200:
+            with open(self.output_file, "wb") as f:
+                f.write(response.content)
+            return self.output_file
+        else:
+            raise Exception(
+                f"Failed to download file from URL: {self.url}, status code: {response.status_code}"
+            )
+
+    def download_from_file_path(self):
+        shutil.copy(self.file_path, self.output_file)
+        return self.output_file
+
+    def download(self):
+        if self.model_id is not None:
+            return self.download_by_model_id()
+        if self.issue_number is not None:
+            return self.download_by_issue_number()
+        if self.url is not None:
+            return self.download_from_url()
+        if self.file_path is not None():
+            return self.download_from_file_path()
+        return None
 
 
-class MetadataLLM(object):
-    def __init__(self, model_id):
+class PublicationSummaryDownloader(object):
+    def __init__(self, model_id=None, url=None, file_path=None):
+        if model_id is None and url is None and file_path is None:
+            raise Exception("At least one argument is necessary")
+        if model_id is not None:
+            if url is not None or file_path is not None:
+                raise Exception("Only one argument is accepted")
+        if url is not None:
+            if model_id is not None or file_path is not None:
+                raise Exception("Only one argument is accepted")
+        if file_path is not None:
+            if model_id is not None or url is not None:
+                raise Exception("Only one argument is accepted")
         self.model_id = model_id
+        self.url = url
+        self.file_path = file_path
+        self.tmp_folder = tempfile.mkdtemp(prefix="ersilia-")
+        self.output_file = os.path.join(self.tmp_folder, "input_file.txt")
 
-    
+    def download_by_model_id(self):
+        s3 = boto3.client("s3")
+        bucket_name = "publication-summaries"  # TODO check path in S3
+        key = f"{self.model_id}_summary.md"
+        try:
+            with open(self.output_file, "wb") as f:
+                s3.download_fileobj(bucket_name, key, f)
+            return self.output_file
+        except Exception as e:
+            raise Exception(f"Failed to download file from S3: {str(e)}")
+
+    def download_from_url(self):
+        response = requests.get(self.url)
+        if response.status_code == 200:
+            with open(self.output_file, "wb") as f:
+                f.write(response.content)
+            return self.output_file
+        else:
+            raise Exception(
+                f"Failed to download file from URL: {self.url}, status code: {response.status_code}"
+            )
+
+    def download_from_file_path(self):
+        shutil.copy(self.file_path, self.output_file)
+        return self.output_file
+
+    def download(self):
+        if self.model_id is not None:
+            return self.download_by_model_id()
+        if self.url is not None:
+            return self.download_from_url()
+        if self.file_path is not None():
+            return self.download_from_file_path()
+        return None
+
+
+class MetadataSuggestor(object):
+    def __init__(self, metadata_txt, publication_markdown, output_markdown):
+        self.model = MODEL_NAME
+        self.metadata_txt = metadata_txt
+        self.publication_markdown = publication_markdown
+        self.output_markdown = output_markdown
+
+    def make_primary_request(self):
+        with open(self.metadata_txt, "r") as f:
+            metadata_txt = f.read()
+        with open(self.publication_markdown, "r") as f:
+            publication_markdown = f.read()
+        system_prompt = PRIMARY_SYSTEM_PROMPT
+        user_prompt = PRIMARY_USER_PROMPT.strip()
+        user_prompt = user_prompt.replace("<RAW_METADATA>", metadata_txt)
+        user_prompt = user_prompt.replace("<PUBLICATION_REPORT>", publication_markdown)
+        response = openai.chat.completions.create(
+            model=self.model_name,
+            messages=[
+                {"role": "system", "content": system_prompt},
+                {"role": "user", "content": user_prompt},
+            ],
+            temperature=0.7,
+        )
+        return response.choices[0].message.content
+
+    def postprocess_markdown(self, text, output_md):
+        system_prompt = POSTPROCESS_SYSTEM_PROMPT.strip()
+        user_prompt = POSTPROCESS_USER_PROMPT.strip() + "\n" + text
+        response = openai.chat.completions.create(
+            model=self.model_name,
+            messages=[
+                {"role": "system", "content": system_prompt},
+                {"role": "user", "content": user_prompt},
+            ],
+            temperature=0.7,
+        )
+        text = response.choices[0].message.content
+        with open(output_md, "w") as f:
+            f.write(text)
+
+    def run(self):
+        text = self.make_primary_request()
+        self.postprocess_markdown(text, self.output_markdown)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Process arguments.")
+    parser.add_argument(
+        "--model_id", type=str, default=None, help="Ersilia Model Hub identifier"
+    )
+    parser.add_argument(
+        "--issue_number",
+        type=int,
+        default=None,
+        help="GitHub issue number in the ersilia-os/ersilia repository",
+    )
+    parser.add_argument(
+        "--url", type=str, default=None, help="URL of the downloadable file"
+    )
+    parser.add_argument("--file_path", type=str, default=None, help="File path")
+    parser.add_argument(
+        "--output_markdown",
+        type=str,
+        default=None,
+        required=True,
+        help="Output file in Markdown format",
+    )
+    args = parser.parse_args()
+    model_id = args.model_id
+    issue_number = args.issue_number
+    url = args.url
+    file_path = args.file_path
+    output_markdown = args.output_markdown
+    md = MetadataDownloader(model_id, issue_number, url, file_path)
+    metadata_txt = md.download()
+    pd = PublicationSummaryDownloader(model_id, url, file_path)
+    publication_markdown = pd.download()
+    ms = MetadataSuggestor(metadata_txt, publication_markdown, output_markdown)
+    ms.run()
--- .github/scripts/summarize_publication_with_llm.py
+++ .github/scripts/summarize_publication_with_llm.py
@@ -1,15 +1,32 @@
 import os
-import sys
+import argparse
 import tempfile
+import PyPDF2
+import openai
 import requests
+import boto3
+import shutil
 from dotenv import load_dotenv
 
 load_dotenv()
 
+# Authenticate into OpenAI
 OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
-AWS_ACCESS_KEY_ID = os.getenv("AWS_ACCESS")
+openai.api_key = OPENAI_API_KEY
+MODEL_NAME = "gpt-4o"
 
-model_id = sys.argv[1]
+# Authenticate into AWS
+aws_access_key_id = os.getenv("AWS_ACCESS_KEY")
+aws_secret_access_key = os.getenv("AWS_SECRET_ACCESS_KEY")
+
+if aws_access_key_id and aws_secret_access_key:
+    boto3.setup_default_session(
+        aws_access_key_id=aws_access_key_id,
+        aws_secret_access_key=aws_secret_access_key,
+    )
+else:
+    raise Exception("AWS credentials are not set in the environment variables")
+
 
 PRIMARY_SYSTEM_PROMPT = """
 Your are a biomedical expert. You have to make a structured report of a scientific publication. The publication will be provided as a PDF file.
@@ -84,6 +101,8 @@ The impact factor of the journal is an important factor for relevance. Higher re
 Do not be over-emphatic. Try to be explicit about the high, medium or low relevance of the publication. Not all publications are highly relevant, so do a fair assessment.
 This should be between 50 and 100 words. Only one paragraph. No new-line characters. Be concise.
 
+---
+
 Below are some style guidelines for the report:
 - Always use the third person and do not use personal pronouns.
 - Do not start paragraphs with sentences such as "this study", "this publication", "this report" or "the authors". Directly explain the content. For example, do not say "This study develops a method for...". Instead, say "A method was developed for..." or similar.
@@ -99,7 +118,7 @@ Below are some style guidelines for the report:
 """
 
 PRIMARY_USER_PROMPT = """
-Make a structured report of the scientific publication provided as a PDF file.
+Make a structured report of the scientific publication from the PDF file. The following is the extracted text from the PDF: 
 """
 
 POSTPROCESS_SYSTEM_PROMPT = """
@@ -107,7 +126,7 @@ You have to standardize the format of a structured report that may contain forma
 You will be provided with a text file with the report. You need to make sure that the report follows the correct format. Do not make drastic changes.
 The format is as follows:
 
-# Publication details:
+# Publication details
 - Title: Title of the publication
 - Authors: List of authors. For example, G. Turon, M. Duran-Frigola and D. Arora. Always abbreviate the given names.
 - Journal: Name of the journal. For example, Nature Biotechnology.
@@ -150,28 +169,183 @@ One paragraph only. Between 50 and 100 words.
 ---
 
 Below are a few general rules:
-- Rephrase if necessary if the content is not clear or concise, but try not to make drastic changes. Just make the small necessary adjustments to follow the guidelines.
+- Rephrase if necessary if the content is not clear or concise, but do not make big changes. Just make the small necessary adjustments to follow the guidelines.
 - Do not include any special characters, boldface, or italics.
 - Do not include any references or links.
 - The report should be self-contained and not require additional information.
 - It is a Markdown file, so make sure to follow the Markdown syntax. All sections are title sections (#) and the lists are bullet points (-).
 - Avoid using double spaces or double new lines. Use only one space or one new line.
+- Correct any inconsistency. For example, if the model is deemed to be highly relevant in a section, and of medium relevance elsewhere, harmonize this. Be conservative.
 
 Make sure that the report follows the correct format. Do not make drastic changes.
-
 """
 
-POSTPROCESS_SYSTEM_PROMPT = """
-Reformat, if necessary, the publication report provided as a text file.
+POSTPROCESS_USER_PROMPT = """
+Reformat, if necessary, the following publication report:
 """
 
-class PublicationSummarizer(object):
-    def __init__(self, model_id):
+
+class PublicationPDFDownloader(object):
+    def __init__(self, model_id=None, issue_number=None, url=None, file_path=None):
+        if (
+            model_id is None
+            and issue_number is None
+            and url is None
+            and file_path is None
+        ):
+            raise Exception("At least one argument is necessary")
+        if model_id is not None:
+            if issue_number is not None or url is not None or file_path is not None:
+                raise Exception("Only one argument is accepted")
+        if issue_number is not None:
+            if model_id is not None or url is not None or file_path is not None:
+                raise Exception("Only one argument is accepted")
+        if url is not None:
+            if (
+                model_id is not None
+                or issue_number is not None
+                or file_path is not None
+            ):
+                raise Exception("Only one argument is accepted")
+        if file_path is not None:
+            if model_id is not None or issue_number is not None or url is not None:
+                raise Exception("Only one argument is accepted")
         self.model_id = model_id
-        self.tmp_dir = tempfile.mkdtemp()
+        self.issue_number = issue_number
+        self.url = url
+        self.file_path = file_path
+        self.tmp_folder = tempfile.mkdtemp(prefix="ersilia-")
+        self.output_file = os.path.join(self.tmp_folder, "publication.pdf")
 
-    def download_publication_from_s3(self):
+    def download_by_model_id(self):
+        s3 = boto3.client("s3")
+        bucket_name = "model-publications"
+        key = f"{self.model_id}_publication.pdf"
+        try:
+            with open(self.output_file, "wb") as f:
+                s3.download_fileobj(bucket_name, key, f)
+            return self.output_file
+        except Exception as e:
+            raise Exception(f"Failed to download file from S3: {str(e)}")
+
+    def download_by_issue_number(self):
+        # TODO download publication PDF from a TBD repository where publications are stored by issue number
         pass
 
+    def download_from_url(self):
+        response = requests.get(self.url)
+        if response.status_code == 200:
+            with open(self.output_file, "wb") as f:
+                f.write(response.content)
+            return self.output_file
+        else:
+            raise Exception(
+                f"Failed to download file from URL: {self.url}, status code: {response.status_code}"
+            )
+
+    def download_from_file_path(self):
+        shutil.copy(self.file_path, self.output_file)
+        return self.output_file
+
+    def download(self):
+        if self.model_id is not None:
+            return self.download_by_model_id()
+        if self.issue_number is not None:
+            return self.download_by_issue_number()
+        if self.url is not None:
+            return self.download_from_url()
+        if self.file_path is not None:
+            return self.download_from_file_path()
+        return None
+
+
+class PublicationSummarizer(object):
+    def __init__(self, publication_pdf, output_markdown):
+        self.tmp_dir = tempfile.mkdtemp(prefix="ersilia-")
+        self.model_name = MODEL_NAME
+        self.publication_pdf = publication_pdf
+        self.output_markdown = output_markdown
+
+    def extract_text_from_pdf(self, pdf_path):
+        with open(pdf_path, "rb") as file:
+            reader = PyPDF2.PdfReader(file)
+            text = ""
+            for page_num in range(len(reader.pages)):
+                page = reader.pages[page_num]
+                text += page.extract_text()
+        return text
+
+    def make_primary_request(self, extracted_text):
+        system_prompt = PRIMARY_SYSTEM_PROMPT.strip()
+        user_prompt = PRIMARY_USER_PROMPT.strip() + "\n" + extracted_text
+        response = openai.chat.completions.create(
+            model=self.model_name,
+            messages=[
+                {"role": "system", "content": system_prompt},
+                {"role": "user", "content": user_prompt},
+            ],
+            temperature=0.7,
+        )
+        return response.choices[0].message.content
+
+    def postprocess_response(self, text_response, markdown_file):
+        system_prompt = POSTPROCESS_SYSTEM_PROMPT.strip()
+        user_prompt = POSTPROCESS_USER_PROMPT.strip() + "\n" + text_response
+        response = openai.chat.completions.create(
+            model=self.model_name,
+            messages=[
+                {"role": "system", "content": system_prompt},
+                {"role": "user", "content": user_prompt},
+            ],
+            temperature=0.7,
+        )
+        text_response = response.choices[0].message.content
+        with open(markdown_file, "w") as f:
+            f.write(text_response)
+
     def run(self):
-        self.download_publication_from_s3()
+        text = self.extract_text_from_pdf(self.publication_pdf)
+        text_response = self.make_primary_request(text)
+        self.postprocess_response(text_response, self.output_markdown)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Process arguments.")
+    parser.add_argument(
+        "-m", "--model_id", type=str, default=None, help="Ersilia Model Hub identifier"
+    )
+    parser.add_argument(
+        "-i",
+        "--issue_number",
+        type=int,
+        default=None,
+        help="GitHub issue number in the ersilia-os/ersilia repository",
+    )
+    parser.add_argument(
+        "-u", "--url", type=str, default=None, help="URL of the downloadable file"
+    )
+    parser.add_argument(
+        "-f",
+        "--file_path",
+        type=str,
+        default=None,
+        help="File path of the publication PDF",
+    )
+    parser.add_argument(
+        "-o",
+        "--output_markdown",
+        type=str,
+        default=None,
+        required=True,
+        help="Output file in Markdown format",
+    )
+    args = parser.parse_args()
+    model_id = args.model_id
+    issue_number = args.issue_number
+    url = args.url
+    file_path = args.file_path
+    output_markdown = args.output_markdown
+    ppd = PublicationPDFDownloader(model_id, issue_number, url, file_path)
+    publication_pdf = ppd.download()
+    ps = PublicationSummarizer(publication_pdf, output_markdown)
+    ps.run()
