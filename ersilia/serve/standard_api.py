4d4b608c5694ccf0684120e7647d32711f0a7b43
Abel Legese <73869888+Abellegese@users.noreply.github.com>
2024-10-26 17:56:23 +0300

    Fixing build error: Applying the nest-asyncio to handle multiple events

--- ersilia/serve/standard_api.py
+++ ersilia/serve/standard_api.py
@@ -1,3 +1,18 @@
+import subprocess
+import sys
+
+def install_package(package):
+    subprocess.check_call([sys.executable, '-m', 'pip', 'install', package])
+
+try:
+    import nest_asyncio
+except ImportError:
+    print("nest_asyncio not found. Installing...")
+    install_package('nest_asyncio')
+    import nest_asyncio
+
+nest_asyncio.apply()
+
 import os
 import csv
 import json
@@ -278,13 +293,7 @@ class StandardCSVRunApi(ErsiliaBase):
                 h = next(reader)
             if len(h) == 1:
                 self.logger.debug("One column found in input")
-                loop = asyncio.get_event_loop()
-                if not loop.is_running():
-                    result = loop.run_until_complete(self.async_serialize_to_json_one_columns(input_data))
-                else:
-                    task = loop.create_task(self.async_serialize_to_json_one_columns(input_data))
-                    result = loop.run_until_complete(task)
-                return result
+                return asyncio.run(self.async_serialize_to_json_one_columns(input_data))
             elif len(h) == 2:
                 self.logger.debug("Two columns found in input")
                 return self.serialize_to_json_two_columns(input_data=input_data)
