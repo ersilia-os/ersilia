ea745566d79e1ddc1e686ecda056a16c7b6362c5
Ajoke Yusuf <ajokeyusuf10@gmail.com>
2024-11-18 09:41:56 +0100

    Add a positive test unit for _is_smiles when chem is none in test_compound_identifier.py (#1332)
    * Add a positive test for _is_smiles when chem is none in test_compound_identifier.py
    
    * Test for valid SMILES input
    
    ---------
    
    Co-authored-by: Dhanshree Arora <DhanshreeA@users.noreply.github.com>

--- test/test_compound_identifier.py
+++ test/test_compound_identifier.py
@@ -1,5 +1,6 @@
 import pytest
 from ersilia.utils.identifiers.compound import CompoundIdentifier
+from unittest.mock import patch
 
 @pytest.fixture
 def compound_identifier():
@@ -18,8 +19,17 @@ def test_is_input_header_negative(compound_identifier, header):
 def test_is_key_header_positive(compound_identifier, header):
     """Test that valid key headers return True."""
     assert compound_identifier.is_key_header(header) is True
-
+    
 @pytest.mark.parametrize("header", ["id","smiles","inchi","input", "some_header", "random", "header", ""])
 def test_is_key_header_negative(compound_identifier, header):
     assert not compound_identifier.is_key_header(header)
+
+@patch('ersilia.utils.identifiers.compound.CompoundIdentifier._pubchem_smiles_to_inchikey')
+def test_is_smiles_positive_chem_none(mock_pubchem, compound_identifier):
+    compound_identifier.Chem = None
+    mock_pubchem.return_value = "InChIKey"
     
+# Test with a valid SMILES input
+    smiles_string = 'CCO' #Ethanol SMILES
+    assert compound_identifier._is_smiles(smiles_string) is True
+
