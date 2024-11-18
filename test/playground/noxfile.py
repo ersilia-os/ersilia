7128fb9d31e88c4e2a2e234e16c308f91887f858
Abellegese <abellegese5@gmail.com>
2024-11-18 19:56:26 +0300

    Playground with all command at #1368

--- test/playground/noxfile.py
+++ test/playground/noxfile.py
@@ -10,17 +10,20 @@ config = yaml.safe_load(config_path.read_text())
 REPO_URL = "https://github.com/ersilia-os/ersilia.git"
 REPO_DIR = Path("ersilia")
 
+
 def update_yaml_values(new_values: dict):
-    config = yaml.safe_load(config_path.read_text())
-    config.update(new_values)
-    config_path.write_text(yaml.dump(config))
+    existing_config = yaml.safe_load(
+        config_path.read_text()
+    )
+    existing_config.update(new_values)
+    config_path.write_text(yaml.dump(existing_config))
+
 
 def get_python_version():
     return config.get("python_version", "3.10.10")
 
-@nox.session(venv_backend="conda", python=get_python_version())
-def test_cli(session):
-    """Test CLI with initial and updated configurations."""
+
+def install_dependencies(session):
     session.install(
         "pytest",
         "pytest-asyncio",
@@ -30,23 +33,14 @@ def test_cli(session):
         "rich",
     )
 
-    if config.get("use_existing_env", False):
-        logger.info("Using existing environment, skipping setup and installation.")
 
-        session.run("ersilia", "--help", external=True)
-        session.run(
-            "pytest",
-            "commands.py",
-            "-v",
-            silent=False,
-            external=True,
+def setup_ersilia(session):
+    if REPO_DIR.exists() and config.get(
+        "overwrite_ersilia_repo", False
+    ):
+        logger.info(
+            f"Overwriting existing repository directory: {REPO_DIR}"
         )
-        return
-
-    session.conda_install(f"python={get_python_version()}")
-
-    if REPO_DIR.exists() and config.get("overwrite_ersilia_repo", False):
-        logger.info(f"Overwriting existing repository directory: {REPO_DIR}")
         shutil.rmtree(REPO_DIR)
 
     if not REPO_DIR.exists():
@@ -57,91 +51,106 @@ def test_cli(session):
             external=True,
         )
     else:
-        logger.info(f"Using existing repository directory: {REPO_DIR}")
+        logger.info(
+            f"Using existing repository directory: {REPO_DIR}"
+        )
 
     session.chdir(REPO_DIR)
     session.install("-e", ".")
     session.chdir(ORIGINAL_DIR)
 
-    session.run(
-        "ersilia",
-        "--help",
-        external=True,
-    )
 
-    # default run --from_github
-    logger.info(f'CLI test for model: {config.get("model_id")} and {config.get("fetch_flags")}')
-    session.run(
-        "pytest",
-        "commands.py",
-        "-v",
-        silent=False,
-    )
-    
-    # default run --from_dockerhub
-    update_yaml_values({
-        "fetch_flags": "--from_dockerhub"
-    })
+@nox.session(
+    venv_backend="conda", python=get_python_version()
+)
+def setup(session):
+    install_dependencies(session)
+    setup_ersilia(session)
 
-    logger.info(f'CLI test for model: {config.get("model_id")} and {config.get("fetch_flags")}')
-    session.run(
-        "pytest", 
-        "commands.py", 
-        "-v", 
-        silent=False
-    )
-    # CLI test run for auto fetcher decider
-    update_yaml_values({
-        "fetch_flags": ""
-    })
-    
-    logger.info(f'CLI test for model: {config.get("model_id")} and auto fetcher decider')
-    session.run(
-        "pytest", 
-        "commands.py", 
-        "-v", 
-        silent=False
-    )
 
-    logger.info(f"Fetching and Serving Multiple Models: Fetching")
-    update_yaml_values({
-        "runner": "multiple",
-        "cli_type": "fetch",
-        "fetch_flags": "--from_dockerhub"
-    })
-    
-    session.run(
-        "pytest", 
-        "commands.py", 
-        "-v", 
-        silent=False
+@nox.session(
+    venv_backend="conda", python=get_python_version()
+)
+def test_from_github(session):
+    install_dependencies(session)
+    logger.info(
+        f'CLI test for model: {config.get("model_id")} and {config.get("fetch_flags")}'
     )
+    session.run("pytest", "commands.py", "-v", silent=False)
 
-    logger.info(f"Fetching and Serving Multiple Models: Serving")
-    update_yaml_values({
-        "runner": "multiple",
-        "cli_type": "serve"
-    })
-    
-    session.run(
-        "pytest", 
-        "commands.py", 
-        "-v", 
-        silent=False
-    )
 
-    logger.info(f"Standard and Conventional Run: Conventional")
-    update_yaml_values({
-        "runner": "single",
-        "cli_type": "all",
-        "fetch_flags": "--from_dockerhub",
-        "output_file": "files/output_eos9gg2_0.json",
-        "output_redirection": "true"
-    })
-    
-    session.run(
-        "pytest", 
-        "commands.py", 
-        "-v", 
-        silent=False
+@nox.session(
+    venv_backend="conda", python=get_python_version()
+)
+def test_from_dockerhub(session):
+    install_dependencies(session)
+    update_yaml_values({"fetch_flags": "--from_dockerhub"})
+    logger.info(
+        f'CLI test for model: {config.get("model_id")} and {config.get("fetch_flags")}'
     )
+    session.run("pytest", "commands.py", "-v", silent=False)
+
+
+@nox.session(
+    venv_backend="conda", python=get_python_version()
+)
+def test_auto_fetcher_decider(session):
+    install_dependencies(session)
+    update_yaml_values({"fetch_flags": ""})
+    logger.info(
+        f'CLI test for model: {config.get("model_id")} and auto fetcher decider'
+    )
+    session.run("pytest", "commands.py", "-v", silent=False)
+
+
+@nox.session(
+    venv_backend="conda", python=get_python_version()
+)
+def test_fetch_multiple_models(session):
+    install_dependencies(session)
+    update_yaml_values(
+        {
+            "runner": "multiple",
+            "cli_type": "fetch",
+            "fetch_flags": "--from_dockerhub",
+        }
+    )
+    logger.info(
+        f"Fetching and Serving Multiple Models: Fetching"
+    )
+    session.run("pytest", "commands.py", "-v", silent=False)
+
+
+@nox.session(
+    venv_backend="conda", python=get_python_version()
+)
+def test_serve_multiple_models(session):
+    install_dependencies(session)
+    update_yaml_values(
+        {"runner": "multiple", "cli_type": "serve"}
+    )
+    logger.info(
+        f"Fetching and Serving Multiple Models: Serving"
+    )
+    session.run("pytest", "commands.py", "-v", silent=False)
+
+
+@nox.session(
+    venv_backend="conda", python=get_python_version()
+)
+def test_conventional_run(session):
+    """Run pytest for standard and conventional run."""
+    install_dependencies(session)
+    update_yaml_values(
+        {
+            "runner": "single",
+            "cli_type": "all",
+            "fetch_flags": "--from_dockerhub",
+            "output_file": "files/output_eos9gg2_0.json",
+            "output_redirection": "true",
+        }
+    )
+    logger.info(
+        f"Standard and Conventional Run: Conventional"
+    )
+    session.run("pytest", "commands.py", "-v", silent=False)
