b07515a5b3b184b9e3efab4faec7862fd2a9a490
Dhanshree Arora <DhanshreeA@users.noreply.github.com>
2024-12-04 18:42:04 +0530

    Generalize Standard Run  (#1411)
    * Modify header calculation to choose from predefined example output file or standard example output file
    
    * Remove the readiness function from SCRA because it is redundant, since those checks are also performed by the amenable function
    
    * Remove unused method
    
    * Make csv serialization work for any kind of model api response
    
    * Remove the standard flag from the CLI since it is now the default run
    
    * Update tests

--- ersilia/cli/commands/run.py
+++ ersilia/cli/commands/run.py
@@ -41,7 +41,11 @@ def run_cmd():
     @click.option(
         "-b", "--batch_size", "batch_size", required=False, default=100, type=click.INT
     )
-    @click.option("--table", is_flag=True, default=False)
+    @click.option(
+        "--table", 
+        is_flag=True, 
+        default=False
+    )
     def run(input, output, batch_size, table):
         session = Session(config_json=None)
         model_id = session.current_model_id()
@@ -76,7 +80,7 @@ def run_cmd():
                     if table:
                         print_result_table(formatted)
                     else:
-                        echo(formatted)
+                        echo(formatted) 
                 else:
                     echo("Something went wrong", fg="red")
         else:
@@ -84,8 +88,7 @@ def run_cmd():
                 print_result_table(result)
             else:
                 try:
-                    echo(result)
+                 echo(result)
                 except:
                     print_result_table(result)
-
     return run
