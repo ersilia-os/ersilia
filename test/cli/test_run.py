220999fa83b263fd08e7061375ccff5a44690bc1
Abellegese <abellegese5@gmail.com>
2024-12-10 00:28:08 +0300

    Unnecessary files removed

--- test/cli/test_run.py
+++ test/cli/test_run.py
@@ -26,16 +26,20 @@ HEADER = ["key", "input", "value"]
 @pytest.fixture
 def mock_fetcher():
     with patch(
-        "ersilia.hub.fetch.fetch.ModelFetcher.fetch", new_callable=AsyncMock
+        "ersilia.hub.fetch.fetch.ModelFetcher.fetch", 
+        new_callable=AsyncMock
     ) as mock_fetch:
         yield mock_fetch
 
 
 @pytest.fixture
 def mock_set_apis():
-    with patch.object(ErsiliaModel, "_set_apis", return_value=None) as mock_set_apis:
+    with patch.object(
+        ErsiliaModel, 
+        "_set_apis", 
+        return_value=None
+        ) as mock_set_apis:
         yield mock_set_apis
-
 
 @pytest.fixture
 def mock_convn_api_get_apis():
@@ -47,19 +51,21 @@ def mock_convn_api_get_apis():
     ) as mock_get_apis:
         yield mock_get_apis
 
-
 @pytest.fixture
 def mock_get_url():
     with patch.object(
-        ErsiliaModel, "_get_url", return_value=f"{URL}:{PORT}"
-    ) as mock_url:
+        ErsiliaModel, 
+        "_get_url", 
+        return_value=f"{URL}:{PORT}"
+        ) as mock_url:
         yield mock_url
-
 
 @pytest.fixture
 def mock_get_input():
     with patch.object(
-        StandardCSVRunApi, "_read_field_from_metadata", return_value=["Compound"]
+        StandardCSVRunApi, 
+        "_read_field_from_metadata",
+          return_value=["Compound"]
     ) as mock_input_type:
         yield mock_input_type
 
@@ -67,44 +73,52 @@ def mock_get_input():
 @pytest.fixture
 def mock_std_header():
     with patch.object(
-        StandardCSVRunApi, "get_expected_output_header", return_value=HEADER
+        StandardCSVRunApi, 
+        "get_expected_output_header", 
+        return_value=HEADER
     ) as mock_method:
         yield mock_method
-
 
 @pytest.fixture
 def mock_is_amenable():
     with patch.object(
-        StandardCSVRunApi, "is_amenable", return_value=True
+        StandardCSVRunApi, 
+        "is_amenable", 
+        return_value=True
     ) as mock_is_amenable:
         yield mock_is_amenable
-
 
 @pytest.fixture
 def compound_csv():
     create_compound_input_csv(INPUT_CSV)
     yield INPUT_CSV
 
-
 @pytest.fixture
 def mock_std_api_post():
     def mock_post_side_effect(input, output, output_source):
-        api_instance = StandardCSVRunApi(model_id=MODEL_ID, url=URL)
+        api_instance = StandardCSVRunApi(
+            model_id=MODEL_ID, 
+            url=URL
+        )
         logger.info(f"Input: {input}")
         input_data = api_instance.serialize_to_json(input)
 
         logger.info(f"Serialized Input Data: {input_data}")
 
         return_value = [
-            {"value": round(random.uniform(MIN_WEIGHT, MAX_WEIGHT), 4)}
+            {"value": round(random.uniform(MIN_WEIGHT, MAX_WEIGHT), 4)} 
             for _ in input_data
         ]
 
         try:
-            time.sleep(0.5)
-            csv_result = api_instance.serialize_to_csv(input_data, return_value, output)
+            time.sleep(0.5) 
+            csv_result = api_instance.serialize_to_csv(
+                input_data, 
+                return_value, 
+                output
+            )
             logger.info(f"CSV Result: {csv_result}")
-
+            
             return csv_result
         except Exception as e:
             logger.error(f"Exception in serialize_to_csv: {e}")
@@ -126,7 +140,6 @@ def mock_session(compound_csv):
     ):
         yield
 
-
 # For Standard API
 def test_standard_api_string(
     mock_std_api_post,
@@ -136,13 +149,15 @@ def test_standard_api_string(
     mock_std_header,
     mock_is_amenable,
     mock_get_url,
-    mock_session,
+    mock_session
 ):
     runner = CliRunner()
 
-    input_arg = INPUT
+    input_arg = INPUT      
     output_arg = RESULT_CSV
-    result = runner.invoke(run_cmd(), ["-i", input_arg, "-o", str(output_arg)])
+    result = runner.invoke(
+        run_cmd(), ["-i", input_arg, "-o", str(output_arg)]
+    )
     assert result.exit_code == 0
     assert mock_get_url.called
     assert mock_get_input.called
@@ -162,12 +177,14 @@ def test_standard_api_csv(
     mock_std_header,
     mock_is_amenable,
     mock_get_url,
-    mock_session,
+    mock_session
 ):
     runner = CliRunner()
     input_arg = INPUT_CSV
     output_arg = RESULT_CSV
-    result = runner.invoke(run_cmd(), ["-i", input_arg, "-o", output_arg])
+    result = runner.invoke(
+        run_cmd(), ["-i", input_arg, "-o", output_arg]
+    )
 
     assert result.exit_code == 0
     assert mock_get_input.called
@@ -176,7 +193,6 @@ def test_standard_api_csv(
     assert mock_is_amenable.called
     assert mock_set_apis.called
     assert RESULT_CSV in result.output
-
 
 # For Conventional Run
 @pytest.fixture
@@ -212,21 +228,28 @@ def test_conv_api_string(
     input_arg = INPUT
     output_arg = RESULT_CSV
     batch_size = 10
-    result = runner.invoke(run_cmd(), ["-i", input_arg, "-b", str(batch_size)])
+    result = runner.invoke(
+        run_cmd(), ["-i", input_arg, "-b", str(batch_size)]
+    )
 
     assert result.exit_code == 0
     assert mock_convn_api_get_apis.called
     assert mock_set_apis.called
 
-
 def test_conv_api_csv(
-    mock_convn_api_get_apis, mock_api_task, mock_set_apis, mock_fetcher, mock_session
+    mock_convn_api_get_apis, 
+    mock_api_task,
+    mock_set_apis,
+    mock_fetcher,
+    mock_session
 ):
     runner = CliRunner()
     input_arg = INPUT_CSV
     output_arg = RESULT_CSV
     batch_size = 10
-    result = runner.invoke(run_cmd(), ["-i", input_arg, "-b", str(batch_size)])
+    result = runner.invoke(
+        run_cmd(), ["-i", input_arg, "-b", str(batch_size)]
+    )
     logger.info(result.output)
     assert result.exit_code == 0
     assert mock_convn_api_get_apis.called
