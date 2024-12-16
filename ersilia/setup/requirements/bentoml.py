d624c2d9c0c05bd0818c16f4ede9730cdca11ec6
Abel Legese <73869888+Abellegese@users.noreply.github.com>
2024-12-16 12:28:06 +0300

    Add detailed docstrings and usage examples to Ersilia'a setup modules (#1442)
    * Merge sample command with the example command (#1422)
    
    * Merge sample command with the example command
    
    * Fix example command usage
    
    * Generalize Standard Run  (#1411)
    
    * Modify header calculation to choose from predefined example output file or standard example output file
    
    * Remove the readiness function from SCRA because it is redundant, since those checks are also performed by the amenable function
    
    * Remove unused method
    
    * Make csv serialization work for any kind of model api response
    
    * Remove the standard flag from the CLI since it is now the default run
    
    * Update tests
    
    * Unnecessary files removed
    
    * Unnecessary files removed
    
    * Unnecessary files removed
    
    * Unnecessary files removed
    
    * Unnecessary files removed
    
    * Some import cleanup
    
    * Few code fixes
    
    * Few code fixes
    
    * Add detailed docstrings and usage examples to Ersilia'a setup modules
    
    ---------
    
    Co-authored-by: Dhanshree Arora <DhanshreeA@users.noreply.github.com>

--- ersilia/setup/requirements/bentoml.py
+++ ersilia/setup/requirements/bentoml.py
@@ -6,18 +6,45 @@ from ...default import EOS
 
 
 class BentoMLRequirement(object):
+    """
+    A class to handle the installation and version checking of BentoML for Ersilia.
+
+    Methods
+    -------
+    is_installed()
+        Checks if BentoML is installed.
+    is_bentoml_ersilia_version()
+        Checks if the installed BentoML version is the Ersilia version.
+    install()
+        Installs the Ersilia version of BentoML.
+    """
     def __init__(self):
         pass
 
-    def is_installed(self):
+    def is_installed(self) -> bool:
+        """
+        Checks if BentoML is installed.
+
+        Returns
+        -------
+        bool
+            True if BentoML is installed, False otherwise.
+        """
         try:
             import bentoml
-
             return True
         except ImportError:
             return False
 
-    def is_bentoml_ersilia_version(self):
+    def is_bentoml_ersilia_version(self) -> bool:
+        """
+        Checks if the installed BentoML version is the Ersilia version.
+
+        Returns
+        -------
+        bool
+            True if the installed BentoML version is the Ersilia version, False otherwise.
+        """
         if not self.is_installed():
             return False
 
@@ -34,7 +61,16 @@ class BentoMLRequirement(object):
         else:
             return False
 
-    def install(self):
+    def install(self) -> None:
+        """
+        Installs the Ersilia version of BentoML.
+
+        This method installs the BentoML package from the Ersilia GitHub repository.
+
+        Returns
+        -------
+        None
+        """
         print("Installing bentoml (the ersilia version)")
         cmd = "{0} -m pip install -U git+https://github.com/ersilia-os/bentoml-ersilia.git".format(
             sys.executable
