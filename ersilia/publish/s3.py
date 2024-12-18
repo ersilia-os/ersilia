b092c75ce83995e9fbee0561f516fdad323e0eff
Abel Legese <73869888+Abellegese@users.noreply.github.com>
2024-12-18 12:55:53 +0300

    Update Sphinx Documentation with Theme, Layout, Functionality Improvements and Page cleanup (#1450)
    * Merge sample command with the example command (#1422)
    
    * Merge sample command with the example command
    
    * Fix example command usage
    
    * Generalize Standard Run  (#1411)
    
    * Modify header calculation to choose from predefined example output file or standard example output file
    
    * Remove the readiness function from SCRA because it is redundant, since those checks are also performed by the amenable function
    
    * Remove unused method
    
    * Make csv serialization work for any kind of model api response
    
    * Remove the standard flag from the CLI since it is now the default run
    
    * Update tests
    
    * Unnecessary files removed
    
    * Unnecessary files removed
    
    * Unnecessary files removed
    
    * Unnecessary files removed
    
    * Unnecessary files removed
    
    * Some import cleanup
    
    * docs: update Sphinx with Furo theme, autosummary, rendering improvements and a few cleanup
    
    ---------
    
    Co-authored-by: Dhanshree Arora <DhanshreeA@users.noreply.github.com>

--- ersilia/publish/s3.py
+++ ersilia/publish/s3.py
@@ -40,6 +40,78 @@ class S3BucketRepoUploader(ErsiliaBase):
         self.aws_access_key_id = None
         self.aws_secret_access_key = None
         self.ignore = ["upload_model_to_s3.py"]
+    
+    def _clone(self):
+        self.logger.debug("Cloning model {0} from ersilia-os".format(self.model_id))
+        run_command(
+            "cd {0}; git clone https://github.com/ersilia-os/{1}; cd {2}".format(
+                self.tmp_folder, self.model_id, self.cwd
+            )
+        )
+
+    def _ungit(self, repo_path):
+        self.logger.debug("Removing git files")
+        dotgit_folder = os.path.join(repo_path, ".git")
+        gitignore_file = os.path.join(repo_path, ".gitignore")
+        github_file = os.path.join(repo_path, ".github")
+        if os.path.exists(dotgit_folder):
+            shutil.rmtree(dotgit_folder)
+        if os.path.exists(gitignore_file):
+            os.remove(gitignore_file)
+        if os.path.exists(github_file):
+            shutil.rmtree(github_file)
+
+    def _upload_files(self, repo_path):
+        self.logger.debug("Uploading repo files")
+        repo_path = os.path.abspath(repo_path)
+        basename = os.path.basename(repo_path)
+        self.logger.debug("Taking basename {0}".format(basename))
+        session = boto3.Session(
+            aws_access_key_id=self.aws_access_key_id,
+            aws_secret_access_key=self.aws_secret_access_key,
+            region_name=AWS_ACCOUNT_REGION,
+        )
+        self.logger.debug(session)
+        s3 = session.resource("s3")
+        bucket = s3.Bucket(ERSILIA_MODELS_S3_BUCKET)
+        self._delete_model_from_s3(bucket)
+        model_id = self.model_id
+        for subdir, _, files in os.walk(repo_path):
+            for file in files:
+                if file in self.ignore:
+                    self.logger.debug("Ignoring {0}".format(file))
+                    continue
+                full_path = os.path.join(subdir, file)
+                with open(full_path, "rb") as data:
+                    s = full_path.split(basename)[-1]
+                    if not s.startswith("/"):
+                        s = "/" + s
+                    self.logger.debug(s)
+                    key = model_id + s
+                    self.logger.debug(key)
+                    bucket.put_object(Key=key, Body=data, ACL="public-read")
+
+    def _delete_model_from_s3(self, bucket):
+        bucket.objects.filter(Prefix="{0}/".format(self.model_id)).delete()
+
+    def _zipdir(self, repo_path, ziph):
+        for root, dirs, files in os.walk(repo_path):
+            for file in files:
+                if file in self.ignore:
+                    continue
+                ziph.write(
+                    os.path.join(root, file),
+                    os.path.relpath(
+                        os.path.join(root, file), os.path.join(repo_path, "..")
+                    ),
+                )
+                
+    def _zip_model(self, repo_path):
+        repo_path = os.path.abspath(repo_path)
+        self.zip_model_file = os.path.join(self.tmp_zip_folder, self.model_id + ".zip")
+        zipf = zipfile.ZipFile(self.zip_model_file, "w", zipfile.ZIP_DEFLATED)
+        self._zipdir(repo_path, zipf)
+        zipf.close()
 
     def set_credentials(self, aws_access_key_id: str, aws_secret_access_key: str):
         """
