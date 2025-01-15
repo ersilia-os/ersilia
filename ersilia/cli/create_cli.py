34d131fee3b44d11eaa11cfd02bfae5b151261df
Oluwadamisi Ayinde <oluwadamisiayinde@gmail.com>
2025-01-15 05:24:03 +0100

    Fixing: issue #1501

--- ersilia/cli/create_cli.py
+++ ersilia/cli/create_cli.py
@@ -1,6 +1,9 @@
+import sys
+
 from ..auth.auth import Auth
 from .cmd import Command
 from .commands import ersilia_cli
+from .messages import VersionNotSupported
 
 
 def create_ersilia_cli():
@@ -9,12 +12,25 @@ def create_ersilia_cli():
 
     This function initializes the Command class, checks if the user is a contributor,
     and dynamically imports and executes various CLI commands based on the user's role.
+    It also verifies that Python 3.8+ is being used.
 
     Returns
     -------
-    ersilia_cli : module
-        The configured Ersilia CLI module.
+    ersilia_cli : module | None
+        The configured Ersilia CLI module if Python version >= 3.8
+        None if the program exits due to version check failure
+    Raises
+    ------
+    SystemExit
+        If Python version is less than 3.8
+    Notes
+    -----
+    This function will terminate the program with sys.exit(1) if Python version < 3.8
     """
+    # Check Python version
+    if sys.version_info.major < 3 or sys.version_info.minor < 8:
+        VersionNotSupported(sys.version.major, sys.version.minor)
+
     is_contributor = Auth().is_contributor()
 
     cmd = Command()
