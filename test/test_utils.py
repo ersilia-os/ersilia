afdc23d4df2bb7c50a299808fdc0b3169c87441b
Olawumi Salaam <olawumisalaam@gmail.com>
2025-01-18 13:09:05 +0100

    unit test for truncate_output function

--- test/test_utils.py
+++ test/test_utils.py
@@ -1,5 +1,6 @@
-import pytest
+import json
 from ersilia.utils.terminal import truncate_output
+
 
 def test_truncate_output_list():
     # Test truncation for a long list
@@ -7,13 +8,13 @@ def test_truncate_output_list():
     truncated = truncate_output(output, max_items=10)
     assert truncated == "[0, 1, 2, 3, 4, 5, 6, 7, 8, 9] ... (and 10 more items)"
 
+
 def test_truncate_output_dict():
     # Test truncation for a long dictionary
     output = {f"key{i}": i for i in range(20)}  # Dictionary with 20 key-value pairs
     truncated = truncate_output(output, max_items=5)
-    assert truncated.startswith("{\n    \"key0\": 0,")
+    assert truncated.startswith('{\n    "key0": 0,')
     assert truncated.endswith("... (and 15 more lines)")
-
 
 
 def test_truncate_output_short_list():
@@ -22,6 +23,7 @@ def test_truncate_output_short_list():
     truncated = truncate_output(output, max_items=10)
     assert truncated == "[1, 2, 3]"
 
+
 def test_truncate_output_short_dict():
     # Test for a short dictionary that doesn't need truncation
     output = {"key1": 1, "key2": 2}
@@ -29,11 +31,13 @@ def test_truncate_output_short_dict():
     assert "key1" in truncated
     assert "key2" in truncated
 
+
 def test_truncate_output_short_string():
     # Test for a short string that doesn't need truncation
     output = "Short string"
     truncated = truncate_output(output, max_chars=50)
     assert truncated == "Short string"
+
 
 def test_truncate_output_other_types():
     # Test for non-list, non-dict, non-string types
