0cfc9cd5aaa1b71e2b5867977b7b274bdfa8680d
miquelduranfrigola <miquelduranfrigola@gmail.com>
2025-02-24 16:47:16 +0100

    bentoml used only when necessary

--- ersilia/setup/requirements/__init__.py
+++ ersilia/setup/requirements/__init__.py
@@ -3,6 +3,8 @@ from .bentoml_requirement import BentoMLRequirement
 
 def check_bentoml():
     req = BentoMLRequirement()
+    if not req.is_installed():
+        req.install()
     if not req.is_bentoml_ersilia_version():
         req.install()
     return
