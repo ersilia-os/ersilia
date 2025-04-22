d96620878c0ba1526add011fc1313cf8922193fc
Abellegese <abellegese5@gmail.com>
2025-04-22 16:07:08 +0300

    fixing assert issue

--- test/collective/test_api.py
+++ test/collective/test_api.py
@@ -8,6 +8,7 @@ import csv
 INPUT_CSV = "input.csv"
 OUTPUT_CSV = "output.csv"
 MODEL_ID = "eos3b5e"
+MODEL_EXIST_MESSAGE = "Model already exists on your system. If you want to fetch it again, please delete the existing model first."
 
 def fetch():
     mf = ModelFetcher(
@@ -35,9 +36,11 @@ def compound_csv():
 
 def test_api(compound_csv):
     is_fetched = fetch()
-    assert is_fetched is True
+    print(is_fetched)
     model = ErsiliaModel(model=MODEL_ID, verbose=True, service_class="conda")
     model.serve()
     model.run(input=INPUT_CSV, output=OUTPUT_CSV)
     has_contents = simple_csv_content_check(OUTPUT_CSV)
+    
+    assert is_fetched.fetch_success is True or MODEL_EXIST_MESSAGE in is_fetched.reason
     assert has_contents is True
