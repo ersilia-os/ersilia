5d97fb38a2031d3648a36ec776b0e34aa6861add
GemmaTuron <gemma@ersilia.io>
2025-05-07 10:09:01 +0200

    remove pack method from metadata

--- ersilia/publish/test/services/checks.py
+++ ersilia/publish/test/services/checks.py
@@ -336,15 +336,6 @@ class CheckService:
         else:
             raise texc.EmptyKey(key)
 
-    def _check_model_pack_method(self, data):
-        key = "Pack Method"
-        self.logger.debug(f"Checking {key}  field..")
-        if key in data:
-            if not data[key]:
-                raise texc.EmptyField(key)
-        else:
-            raise texc.EmptyKey(key)
-
     def _check_model_image_size(self, data):
         key = "Image Size"
         self.logger.debug(f"Checking {key}  field..")
@@ -462,7 +453,6 @@ class CheckService:
         self._run_check(
             self._check_model_contribution_date, data, "Model Contribution Date"
         )
-        self._run_check(self._check_model_pack_method, data, "Model Pack Method")
         self._run_check(self._check_model_image_size, data, "Model Image Size")
         self._run_check(self._check_model_env_size, data, "Model Environment Size")
         self._run_check(self._check_model_model_size, data, "Model Directory Size")
