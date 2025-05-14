5968e80ec9649c8ab9501491f902c932b55accf8
Miquel Duran-Frigola <miquelduranfrigola@gmail.com>
2025-05-14 18:47:58 +0200

    keep skipping

--- ersilia/publish/test/services/checks.py
+++ ersilia/publish/test/services/checks.py
@@ -748,7 +748,7 @@ class CheckService:
                 )
 
         def check_h5():
-            return (input_type.upper(), "HDF5 tested skipped", str(STATUS_CONFIGS.SUCCESS))
+            return (input_type.upper(), "HDF5 tested skipped", str(STATUS_CONFIGS.PASSED))
             self.logger.debug(f"Checking HDF5 file: {file_path}")
             error_details = []
             metadata = self.ios._read_metadata()
