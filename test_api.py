e1e4faa7f8f0802fd0aaf88f2f94af2f14945992
patrickwalsh26 <walshp26@stanford.edu>
2025-07-07 13:00:46 -0500

    test_api

--- test_api.py
+++ test_api.py
@@ -1,15 +1,6 @@
 from ersilia.api import ErsiliaAPI
 
-<<<<<<< HEAD
-
 mdl = ErsiliaAPI()
 mdl.serve()
 example_input = mdl.example(n = 10)
 df = mdl.run(example_input, batch_size = 100)
-=======
-mdl = ErsiliaAPI("eo3sb5e")
-mdl.serve()
-input = ["CCCCO"]
-# example_input = mdl.example(n = 10)
-df = mdl.run(input, 100)
->>>>>>> 228034391cf47b9e7984dbc1ae100bd4ec808cac
