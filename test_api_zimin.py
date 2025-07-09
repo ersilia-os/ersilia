f96f467dce1664742d64c62a30a68fd75eb10423
ziminqian <ziminq@stanford.edu>
2025-07-09 10:59:40 -0700

    test two models

--- test_api_zimin.py
+++ test_api_zimin.py
@@ -1,9 +1,11 @@
 from ersilia.api.create_api import ErsiliaAPI
 
-mdl = ErsiliaAPI("eos8ub5")
+mdl = ErsiliaAPI("eos3b5e")
 mdl.serve()
-input = ["CCCCO", "CCO"]
+input = ["CCCCO", "C", "CC"]
 df = mdl.run(input, 100)
+mdl.info()
+mdl.example(5, True, True)
 mdl.close()
 mdl.delete()
 
