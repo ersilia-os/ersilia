e7e212fece7c99c2e7818410a30920a0fc6f9166
ziminqian <ziminq@stanford.edu>
2025-07-10 10:32:26 -0700

    example docstring

--- ersilia/api/commands/example.py
+++ ersilia/api/commands/example.py
@@ -5,12 +5,30 @@ from ...core.session import Session
 from ...io.input import ExampleGenerator
 from ..echo import echo
 
-"""Create example command"""
 
+def example(model, file_name, simple=True, random=True, n_samples=5, deterministic=False):  
+    """
+    This command can sample inputs for a given model. 
+    
+    Args
+    -------
+    model: The model ID to be served. Can either be the eos identifier or the slug identifier.
+    file_name: File name where the examples should be saved.
+    simple: Simple inputs only contain the SMILES, while complete inputs also include InChIKey and the molecule's name.
+    random: If the model source contains an example input file, when the predefined flag is set, then inputs are sampled from that file. Only the number of samples present in the file are returned, especially if --n_samples is greater than that number. By default, Ersilia samples inputs randomly.
+    n_samples: Specify the number of example inputs to generate for the given model.
+    deterministic: Used to generate examples data deterministically instead of random sampling. This allows when every time you run with example command with this flag you get the same types of examples. 
 
-def example(
-    model, file_name, simple=True, random=True, n_samples=5, deterministic=False
-):  # model, file_name, simple, random, n_samples = 5, deterministic = False
+    Returns
+    -------
+    Function: The fetch command function to be used by the CLI and for testing in the pytest.
+    Str: Confirmation message on success or warning message on failure.
+    
+    Raises
+    -------
+    RuntimeError: If both BentoML and FastAPI are used togehter. 
+
+    """
     if model is not None:
         model_id = ModelBase(model).model_id
     else:
@@ -46,23 +64,4 @@ def example(
         )
     return example
 
-    # if model is not None:
-    #     model_id = ModelBase(model).model_id
-    # else:
-    #     session = Session(config_json=None)
-    #     model_id = session.current_model_id()
-    # if not model_id:
-    #     raise RuntimeError("No model found. Please specify a model or serve a model in the current shell.", fg="red",)
-    # eg = ExampleGenerator(model_id=model_id)
-    # with tempfile.NamedTemporaryFile(mode="w+", delete=False, suffix=".json") as output:
-    #     output_file_name = output.name
-    #     eg.example(
-    #           n_samples,
-    #           output_file_name,
-    #           try_predefined=not random,
-    #           deterministic=deterministic,
-    #           simple=True
-    #       )
-    #     output.seek(0)
-    #     example_data = json.load(output)
-    # return example_data
+    
