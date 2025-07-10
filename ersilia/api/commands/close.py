d28ec30c2e442a31178f06bc4e07dc4323489037
ziminqian <ziminq@stanford.edu>
2025-07-10 10:38:40 -0700

    close docstring

--- ersilia/api/commands/close.py
+++ ersilia/api/commands/close.py
@@ -6,6 +6,21 @@ from ..echo import echo
 
 
 def close(model_id):
+    """
+    This command closes the current session of the served model and cleans up any associated resources.
+
+    Args
+    -------
+        model_id (str): ID of the model to delete.
+
+    Returns
+    -------
+        str: Confirmation message on success or warning message on failure.
+
+    Raises
+    -------
+        RuntimeError: If no model was served in the current session.
+    """
     session = Session(config_json=None)
     service_class = session.current_service_class()
     if model_id is None:
