78e0b3de2ad7fd842523c5c205ac59165934b77b
ziminqian <ziminq@stanford.edu>
2025-07-21 13:25:23 -0700

    scaling and quantize test

--- test_api_zimin.py
+++ test_api_zimin.py
@@ -2,14 +2,14 @@ from ersilia.api import ErsiliaHub, ErsiliaModel
 
 mdl = ErsiliaModel("eos9p4a") # drug-likeness
 # mdl.is_fetched()
-# mdl.fetch()
-# mdl.serve()
+mdl.fetch()
+mdl.serve()
 # mdl.is_docker()
 # mdl.delete()
 # mdl.is_fetched()
 # mdl.delete()
 # mdl.serve()
-# input = ["CCCCO", "C", "CC"]
+input = ["CCCCO", "C", "CC"]
 # df = mdl.run(input, batch_size=100)
 # df.to_csv("TESTRUN.csv", index=False)
 # molecular_weight = ErsiliaModel("eos3b5e", verbose=False)
@@ -19,13 +19,13 @@ mdl = ErsiliaModel("eos9p4a") # drug-likeness
 # molecular_weight.fetch()
 # molecular_weight.serve()
 
-# input = ["CCCCO", "C", "CC"]
-# df = molecular_weight.run(input, batch_size=100)
+# input = ["CCCCO", "C", x"CC"]
+df = mdl.run(input, batch_size=100)
 # print(df)
 
-Hub = ErsiliaHub()
-df = Hub.catalog()
-print("df =", df)
+# Hub = ErsiliaHub()
+# df = Hub.catalog()
+# print("df =", df)
 #df.to_csv("Catalog.csv", index=False)
 
 #mdl.info()
