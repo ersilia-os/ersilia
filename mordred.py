51779adc1b112ea1eebb79036b5cc077bb4d856d
patrickwalsh26 <walshp26@stanford.edu>
2025-07-29 11:41:46 -0500

    deleted unnecessary

--- mordred.py
+++
@@ -1,6 +0,0 @@
-from ersilia.api import ErsiliaModel
-
-mordred = ErsiliaModel("eos78ao")
-# mordred.fetch()
-# mordred.serve()
-df = mordred.run(input="drugbank_input5.csv", output="drugbank_output5.csv", batch_size=100)
