185c8302606add7fccf40322362ab1c3a00e7ad9
patrickwalsh26 <walshp26@stanford.edu>
2025-08-04 13:05:08 -0500

    removing files

--- mordred.py
+++
@@ -1,7 +0,0 @@
-from ersilia.api import ErsiliaAPIModel
-
-mordred = ErsiliaAPIModel("eos78ao", verbose=True)
-# mordred.fetch()
-# mordred.serve()
-df = mordred.run("drugbank_input5.csv", "drugbank_outputTEST2.csv", batch_size=1000)
-print(df)
