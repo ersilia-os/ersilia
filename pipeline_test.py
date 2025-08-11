626ee5912d1dc00e532d78bc50d0853b893811f1
patrickwalsh26 <walshp26@stanford.edu>
2025-08-11 15:18:38 -0500

    cleaned up

--- pipeline_test.py
+++
@@ -1,8 +0,0 @@
-from ersilia.api import ErsiliaCatalog, ErsiliaModel
-
-mordred = ErsiliaModel("eos78ao")
-catalog_obj = ErsiliaCatalog()
-catalog_obj.catalog()
-print(mordred.is_fetched())
-mordred.delete()
-print(mordred.is_fetched())
