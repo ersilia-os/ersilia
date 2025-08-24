6c936cbac39330b38dfb45e5a17598be128831a8
Miquel Duran-Frigola <miquelduranfrigola@gmail.com>
2025-08-24 18:18:19 +0200

    temporary directory for the example

--- test/playground/utils.py
+++ test/playground/utils.py
@@ -2,6 +2,7 @@ import json
 import os
 import platform
 import psutil
+import shutil
 import pytest
 import requests
 import subprocess
@@ -144,22 +145,27 @@ def get_commands_all(model_id, config):
             f"{cmd.__name__}: Model id not required{flag_description}",
         )
 
-    tmp_file = tempfile.NamedTemporaryFile()
+    tmp_folder = tempfile.mkdtemp(prefix="ersilia-")
+    tmp_file = os.path.join(tmp_folder, "example.csv")
 
-    return {
+    data = {
         "fetch": build_command(
             fetch_cmd, flag_key="fetch", model_id=model_id
         ),
         "serve": build_command(serve_cmd, flag_key="serve", model_id=model_id),
         "run": build_command(run_cmd, flag_key="run"),
         "catalog": build_command(catalog_cmd, flag_key="catalog"),
-        "example": build_command(example_cmd, extra_args={"file_name": tmp_file.name}, flag_key="example"),
+        "example": build_command(example_cmd, extra_args={"file_name": tmp_file}, flag_key="example"),
         "close": build_command(close_cmd),
         "delete": build_command(
             delete_cmd, flag_key="delete", model_id=model_id
         ),
         "test": build_command(test_cmd, flag_key="test", model_id=model_id),
     }
+
+    shutil.rmtree(tmp_folder)
+
+    return data
 
 
 def extract_commands(keys, model_id, config):
