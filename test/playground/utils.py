7185fa565b10c0f492dabe74fccdd92f02973357
Miquel Duran-Frigola <miquelduranfrigola@gmail.com>
2025-08-24 23:49:38 +0200

    example csv not in temporary

--- test/playground/utils.py
+++ test/playground/utils.py
@@ -7,7 +7,6 @@ import requests
 import subprocess
 import click
 import traceback
-import tempfile
 from datetime import datetime
 from pathlib import Path
 from ersilia.default import EOS_PLAYGROUND
@@ -144,8 +143,7 @@ def get_commands_all(model_id, config):
             f"{cmd.__name__}: Model id not required{flag_description}",
         )
 
-    tmp_dir = tempfile.mktemp(prefix="ersilia-")
-    tmp_file = os.path.join(tmp_dir, "example.csv")
+    tmp_file = "tmp_example.csv")
 
     data = {
         "fetch": build_command(
