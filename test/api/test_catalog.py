e4a591beddc038cfa14209d79e37b58489645184
GemmaTuron <gemma@ersilia.io>
2025-08-24 17:26:27 +0200

    fix test catalog

--- test/api/test_catalog.py
+++ test/api/test_catalog.py
@@ -2,11 +2,11 @@ from ersilia.api import Catalog
 
 def test_catalog_hub():
     cat = Catalog()
-    df = cat.catalog()
+    df = cat.catalog(hub=True)
     assert df.shape[0] > 10
 
 def test_catalog_local():
     cat = Catalog()
-    df = cat.catalog()
+    df = cat.catalog(hub=False)
     assert 1 == 1
 
