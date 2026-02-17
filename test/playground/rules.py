d5c13699cec04fd382e83554cdb542409d416258
Abellegese <abellegese5@gmail.com>
2026-02-17 19:30:02 +0300

    content url normilizing and probing

--- test/playground/rules.py
+++ test/playground/rules.py
@@ -24,7 +24,7 @@ from ersilia.utils.session import get_session_dir
 from ersilia.publish.test.services.checks import CheckService
 from ersilia.publish.test.services.io import IOService
 from ersilia.publish.test.services.constants import STATUS_CONFIGS
-from ersilia.utils.ports import normalize_connect_url
+from ersilia.utils.ports import normalize_connect_url, _ensure_ready
 class ResponseName(Enum):
     DEST_FOLDER_EXIST = "Dest Folder Exists"
     DEST_FOLDER_HAS_CONTENT = "Dest folder has necessary files"
@@ -246,6 +246,7 @@ class ServeRule(CommandRule):
         match = re.search(pattern, text)
         url = match.group(0)
         url = normalize_connect_url(url)
+        _ensure_ready(url)
         response = requests.get(url)
         status_ok = response.status_code == 200
         return create_response(name=ResponseName.SESSION_URL_VALID, status=status_ok)
