d7028bff3e83978a4857856cf889b9af3ff39a82
Marina18 <marina@ersilia.io>
2026-05-14 10:58:59 +0200

    fix circular import in download.py by importing logger directly from utils.logging

--- ersilia/utils/download.py
+++ ersilia/utils/download.py
@@ -11,7 +11,7 @@ from pathlib import Path
 
 import requests
 
-from .. import logger
+from .logging import logger
 from ..default import S3_BUCKET_URL
 from ..utils.echo import echo
 from ..utils.logging import make_temp_dir
